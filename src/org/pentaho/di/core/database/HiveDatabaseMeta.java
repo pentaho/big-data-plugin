8c99d7c71afabdd88c939e6cba3a87670b785306
Matt Burgess <mburgess@pentaho.com>
2013-04-18 13:48:56 -0400

    PDI-9293: Added support for fetching EXTERNAL tables in Hive

--- src/org/pentaho/di/core/database/HiveDatabaseMeta.java
+++ src/org/pentaho/di/core/database/HiveDatabaseMeta.java
@@ -256,4 +256,13 @@ public class HiveDatabaseMeta
     public String getEndQuote() {
       return "";
     }
+    
+    /**
+     * @return a list of table types to retrieve tables for the database
+     */
+    @Override
+    public String[] getTableTypes()
+    {
+      return new String[] { "TABLE", "EXTERNAL TABLE" };
+    }
 }
