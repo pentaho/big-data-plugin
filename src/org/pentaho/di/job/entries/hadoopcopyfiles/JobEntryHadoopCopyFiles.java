af10884aee9a0756fc413ebe57ebc1e4a579f333
e-cuellar <ecuellar@pentaho.com>
2015-05-15 12:29:07 -0400

    [BACKLOG-2972] Hadoop Copy Files refactoring and Copy Files Fixes

--- src/org/pentaho/di/job/entries/hadoopcopyfiles/JobEntryHadoopCopyFiles.java
+++ src/org/pentaho/di/job/entries/hadoopcopyfiles/JobEntryHadoopCopyFiles.java
@@ -22,23 +22,14 @@
 
 package org.pentaho.di.job.entries.hadoopcopyfiles;
 
-import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.vfs.FileName;
-import org.apache.commons.vfs.FileSystemException;
-import org.apache.commons.vfs.provider.url.UrlFileNameParser;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.annotations.JobEntry;
-import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.hadoop.HadoopSpoonPlugin;
 import org.pentaho.di.core.namedcluster.NamedClusterManager;
-import org.pentaho.di.core.xml.XMLHandler;
 import org.pentaho.di.job.entries.copyfiles.JobEntryCopyFiles;
-import org.pentaho.di.repository.ObjectId;
-import org.pentaho.di.repository.Repository;
 import org.pentaho.metastore.api.IMetaStore;
-import org.w3c.dom.Node;
 
 @JobEntry( id = "HadoopCopyFilesPlugin", image = "HDM.svg", name = "HadoopCopyFilesPlugin.Name",
     description = "HadoopCopyFilesPlugin.Description",
@@ -47,24 +38,8 @@ import org.w3c.dom.Node;
     documentationUrl = "http://wiki.pentaho.com/display/EAI/Hadoop+Copy+Files" )
 public class JobEntryHadoopCopyFiles extends JobEntryCopyFiles {
 
-  private Map<String, String> namedClusterURLMapping = null;
-
-  public static final String SOURCE_CONFIGURATION_NAME = "source_configuration_name";
-  public static final String SOURCE_FILE_FOLDER = "source_filefolder";
-
-  public static final String DESTINATION_CONFIGURATION_NAME = "destination_configuration_name";
-  public static final String DESTINATION_FILE_FOLDER = "destination_filefolder";
-
-  public static final String LOCAL_SOURCE_FILE = "LOCAL-SOURCE-FILE-";
-  public static final String LOCAL_DEST_FILE = "LOCAL-DEST-FILE-";
-
-  public static final String STATIC_SOURCE_FILE = "STATIC-SOURCE-FILE-";
-  public static final String STATIC_DEST_FILE = "STATIC-DEST-FILE-";
-
   public static final String S3_SOURCE_FILE = "S3-SOURCE-FILE-";
   public static final String S3_DEST_FILE = "S3-DEST-FILE-";
-  
-  private NamedClusterManager namedClusterManager = NamedClusterManager.getInstance();
 
   public JobEntryHadoopCopyFiles() {
     this( "" ); //$NON-NLS-1$
@@ -72,60 +47,10 @@ public class JobEntryHadoopCopyFiles extends JobEntryCopyFiles {
 
   public JobEntryHadoopCopyFiles( String name ) {
     super( name );
-    namedClusterURLMapping = new HashMap<String, String>();
-  }
-
-  protected void saveSource( StringBuilder retval, String source ) {
-    String namedCluster = namedClusterURLMapping.get( source );
-    retval.append( "          " ).append( XMLHandler.addTagValue( SOURCE_FILE_FOLDER, source ) );
-    retval.append( "          " ).append( XMLHandler.addTagValue( SOURCE_CONFIGURATION_NAME, namedCluster ) );
-  }
-
-  protected void saveDestination( StringBuilder retval, String destination ) {
-    String namedCluster = namedClusterURLMapping.get( destination );
-    retval.append( "          " ).append( XMLHandler.addTagValue( DESTINATION_FILE_FOLDER, destination ) );
-    retval.append( "          " ).append( XMLHandler.addTagValue( DESTINATION_CONFIGURATION_NAME, namedCluster ) );
-  }
-
-  protected void saveSourceRep( Repository rep, ObjectId id_job, ObjectId id_jobentry, int i, String sourceFileFolder )
-    throws KettleException {
-    String namedCluster = namedClusterURLMapping.get( sourceFileFolder );
-    rep.saveJobEntryAttribute( id_job, getObjectId(), i, SOURCE_FILE_FOLDER, sourceFileFolder );
-    rep.saveJobEntryAttribute( id_job, id_jobentry, i, SOURCE_CONFIGURATION_NAME, namedCluster );
-  }
-
-  protected void saveDestinationRep( Repository rep, ObjectId id_job, ObjectId id_jobentry, int i,
-      String destinationFileFolder ) throws KettleException {
-    String namedCluster = namedClusterURLMapping.get( destinationFileFolder );
-    rep.saveJobEntryAttribute( id_job, getObjectId(), i, DESTINATION_FILE_FOLDER, destinationFileFolder );
-    rep.saveJobEntryAttribute( id_job, id_jobentry, i, DESTINATION_CONFIGURATION_NAME, namedCluster );
-  }
-
-  protected String loadSourceRep( Repository rep, ObjectId id_jobentry, int a ) throws KettleException {
-    String source_filefolder = rep.getJobEntryAttributeString( id_jobentry, a, SOURCE_FILE_FOLDER );
-    String ncName = rep.getJobEntryAttributeString( id_jobentry, a, SOURCE_CONFIGURATION_NAME );
-    return loadURL( source_filefolder, ncName, getMetaStore(), namedClusterURLMapping );
-  }
-
-  protected String loadSource( Node fnode ) {
-    String source_filefolder = XMLHandler.getTagValue( fnode, SOURCE_FILE_FOLDER );
-    String ncName = XMLHandler.getTagValue( fnode, SOURCE_CONFIGURATION_NAME );
-    return loadURL( source_filefolder, ncName, getMetaStore(), namedClusterURLMapping );
-  }
-
-  protected String loadDestinationRep( Repository rep, ObjectId id_jobentry, int a ) throws KettleException {
-    String destination_filefolder = rep.getJobEntryAttributeString( id_jobentry, a, DESTINATION_FILE_FOLDER );
-    String ncName = rep.getJobEntryAttributeString( id_jobentry, a, DESTINATION_CONFIGURATION_NAME );
-    return loadURL( destination_filefolder, ncName, getMetaStore(), namedClusterURLMapping );
-  }
-
-  protected String loadDestination( Node fnode ) {
-    String destination_filefolder = XMLHandler.getTagValue( fnode, DESTINATION_FILE_FOLDER );
-    String ncName = XMLHandler.getTagValue( fnode, DESTINATION_CONFIGURATION_NAME );
-    return loadURL( destination_filefolder, ncName, getMetaStore(), namedClusterURLMapping );
   }
 
   public String loadURL( String url, String ncName, IMetaStore metastore, Map mappings ) {
+    NamedClusterManager namedClusterManager = NamedClusterManager.getInstance();
     url =
         namedClusterManager.processURLsubstitution( ncName, url, HadoopSpoonPlugin.HDFS_SCHEME, metastore,
             getVariables() );
@@ -133,28 +58,5 @@ public class JobEntryHadoopCopyFiles extends JobEntryCopyFiles {
       mappings.put( url, ncName );
     }
     return url;
-  }
-
-  public void setNamedClusterURLMapping( Map<String, String> mappings ) {
-    this.namedClusterURLMapping = mappings;
-  }
-
-  public Map<String, String> getNamedClusterURLMapping() {
-    return this.namedClusterURLMapping;
-  }
-
-  public String getClusterNameBy( String url ) {
-    return this.namedClusterURLMapping.get( url );
-  }
-
-  public String getUrlPath( String source ) {
-    try {
-      UrlFileNameParser parser = new UrlFileNameParser();
-      FileName fileName = parser.parseUri( null, null, source );
-      source = fileName.getPath();
-    } catch ( FileSystemException e ) {
-      source = null;
-    }
-    return source;
   }
 }
