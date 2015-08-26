05bef049b5f2ce09c24c51b8f7aac8521fc9034b
Matthew Burgess <mburgess@pentaho.com>
2015-08-26 12:18:44 -0400

    [BACKLOG-4505] Added toString() override to NamedClusterImpl

--- impl/cluster/src/test/java/org/pentaho/big/data/impl/cluster/NamedClusterImplTest.java
+++ impl/cluster/src/test/java/org/pentaho/big/data/impl/cluster/NamedClusterImplTest.java
@@ -1,23 +1,21 @@
 /*******************************************************************************
- *
  * Pentaho Big Data
- *
+ * <p/>
  * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
- *
- *******************************************************************************
- *
+ * <p/>
+ * ******************************************************************************
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  ******************************************************************************/
 
 package org.pentaho.big.data.impl.cluster;
@@ -144,7 +142,7 @@ public class NamedClusterImplTest {
   public void testFieldSubstitute() throws KettleValueException {
     String testString = "testString";
     RowMetaInterface rowMetaInterface = mock( RowMetaInterface.class );
-    Object[] rowData = new Object[] {};
+    Object[] rowData = new Object[]{};
     String testVal = "testVal";
     when( variableSpace.fieldSubstitute( testString, rowMetaInterface, rowData ) ).thenReturn( testVal );
     assertEquals( testVal, namedCluster.fieldSubstitute( testString, rowMetaInterface, rowData ) );
@@ -185,7 +183,7 @@ public class NamedClusterImplTest {
 
   @Test
   public void testListVariables() {
-    String[] vars = new String[] { "vars" };
+    String[] vars = new String[]{ "vars" };
     when( variableSpace.listVariables() ).thenReturn( vars );
     assertArrayEquals( vars, namedCluster.listVariables() );
   }
@@ -215,5 +213,13 @@ public class NamedClusterImplTest {
     assertTrue( NamedClusterImpl.comparator.compare( namedCluster, other ) < 0 );
     other.setName( namedClusterName );
     assertTrue( NamedClusterImpl.comparator.compare( namedCluster, other ) == 0 );
+  }
+
+  @Test
+  public void testToString() {
+    NamedClusterImpl other = new NamedClusterImpl();
+    assertEquals( "Named cluster: null", other.toString() );
+    other.setName( "a" );
+    assertEquals( "Named cluster: a", other.toString() );
   }
 }
