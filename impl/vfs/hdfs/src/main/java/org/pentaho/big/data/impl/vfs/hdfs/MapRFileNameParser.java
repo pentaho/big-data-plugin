00db3c9ab4f9e5dd72a88f198b164ee417632100
Dzmitry_Stsiapanau <stepanovdg@gmail.com>
2015-09-14 13:04:22 +0300

    [BACKLOG-5127] - Regression: VFS Browser fails to connect to MapR FS

--- impl/vfs/hdfs/src/main/java/org/pentaho/big/data/impl/vfs/hdfs/MapRFileNameParser.java
+++ impl/vfs/hdfs/src/main/java/org/pentaho/big/data/impl/vfs/hdfs/MapRFileNameParser.java
@@ -24,15 +24,29 @@ package org.pentaho.big.data.impl.vfs.hdfs;
 
 import org.apache.commons.vfs2.provider.URLFileNameParser;
 
-public class HDFSFileNameParser extends URLFileNameParser {
+public class MapRFileNameParser extends URLFileNameParser {
 
-  private static final HDFSFileNameParser INSTANCE = new HDFSFileNameParser();
+  public static final String EMPTY_HOSTNAME = "";
 
-  private HDFSFileNameParser() {
+  private static final MapRFileNameParser INSTANCE = new MapRFileNameParser();
+
+  private MapRFileNameParser() {
     super( -1 );
   }
 
-  public static HDFSFileNameParser getInstance() {
+  public static MapRFileNameParser getInstance() {
     return INSTANCE;
+  }
+
+  /**
+   * Extracts the hostname from a URI.
+   *
+   * @param name string buffer with the "scheme://[userinfo@]" part has been removed already. Will be modified.
+   * @return the host name  or null.
+   */
+  @Override protected String extractHostName( StringBuilder name ) {
+    final String hostname = super.extractHostName( name );
+    // Trick the URLFileNameParser into thinking we have a hostname so we don't have to refactor it.
+    return hostname == null ? EMPTY_HOSTNAME : hostname;
   }
 }
