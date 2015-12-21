pentaho/big-data-plugin

commit ceb855aa6b215b43a82f9b5c353527a29b83d3d7

[BACKLOG-4102] - MapReduce OSGi
Api, shim impl, move legacy plugins to kettle-plugins bundle, assembly update

kettle-plugins/mapreduce/src/main/java/org/pentaho/big/data/kettle/plugins/mapreduce/JobEntryHadoopJobExecutorDialog.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Big Data
  *
- * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -20,24 +20,23 @@
  *
  ******************************************************************************/
 
-package org.pentaho.di.ui.job.entries.hadoopjobexecutor;
-
-import java.util.Enumeration;
-import java.util.List;
-import java.util.ResourceBundle;
+package org.pentaho.big.data.kettle.plugins.mapreduce;
 
 import org.dom4j.DocumentException;
 import org.eclipse.swt.widgets.Shell;
-import org.pentaho.di.core.namedcluster.model.NamedCluster;
+import org.pentaho.big.data.api.cluster.NamedCluster;
+import org.pentaho.big.data.api.cluster.NamedClusterService;
+import org.pentaho.big.data.kettle.plugins.mapreduce.JobEntryHadoopJobExecutorController.AdvancedConfiguration;
+import org.pentaho.big.data.kettle.plugins.mapreduce.JobEntryHadoopJobExecutorController.SimpleConfiguration;
+import org.pentaho.big.data.plugins.common.ui.HadoopClusterDelegateImpl;
 import org.pentaho.di.i18n.BaseMessages;
 import org.pentaho.di.job.JobMeta;
-import org.pentaho.di.job.entries.hadoopjobexecutor.JobEntryHadoopJobExecutor;
 import org.pentaho.di.job.entry.JobEntryDialogInterface;
 import org.pentaho.di.job.entry.JobEntryInterface;
 import org.pentaho.di.repository.Repository;
-import org.pentaho.di.ui.job.entries.hadoopjobexecutor.JobEntryHadoopJobExecutorController.AdvancedConfiguration;
-import org.pentaho.di.ui.job.entries.hadoopjobexecutor.JobEntryHadoopJobExecutorController.SimpleConfiguration;
 import org.pentaho.di.ui.job.entry.JobEntryDialog;
+import org.pentaho.di.ui.spoon.Spoon;
+import org.pentaho.metastore.api.exceptions.MetaStoreException;
 import org.pentaho.ui.xul.XulDomContainer;
 import org.pentaho.ui.xul.XulException;
 import org.pentaho.ui.xul.XulRunner;
@@ -53,14 +52,20 @@ import org.pentaho.ui.xul.containers.XulTree;
 import org.pentaho.ui.xul.containers.XulVbox;
 import org.pentaho.ui.xul.swt.SwtXulLoader;
 import org.pentaho.ui.xul.swt.SwtXulRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class JobEntryHadoopJobExecutorDialog extends JobEntryDialog implements JobEntryDialogInterface {
   private static final Class<?> CLZ = JobEntryHadoopJobExecutor.class;
-
+  private static final Logger logger = LoggerFactory.getLogger( JobEntryHadoopJobExecutorDialog.class );
+  private final NamedClusterService namedClusterService;
+  private final JobEntryHadoopJobExecutorController controller;
   private JobEntryHadoopJobExecutor jobEntry;
-
-  private JobEntryHadoopJobExecutorController controller = new JobEntryHadoopJobExecutorController();
-
   private XulDomContainer container;
 
   private BindingFactory bf;
@@ -80,8 +85,12 @@ public class JobEntryHadoopJobExecutorDialog extends JobEntryDialog implements J
   public JobEntryHadoopJobExecutorDialog( Shell parent, JobEntryInterface jobEntry, Repository rep, JobMeta jobMeta )
     throws XulException, DocumentException, Throwable {
     super( parent, jobEntry, rep, jobMeta );
-
     this.jobEntry = (JobEntryHadoopJobExecutor) jobEntry;
+    this.namedClusterService = this.jobEntry.getNamedClusterService();
+    controller = new JobEntryHadoopJobExecutorController(
+      new HadoopClusterDelegateImpl( Spoon.getInstance(), namedClusterService,
+        this.jobEntry.getRuntimeTestActionService(), this.jobEntry.getRuntimeTester() ), namedClusterService,
+      this.jobEntry.getNamedClusterServiceLocator() );
 
     SwtXulLoader swtXulLoader = new SwtXulLoader();
     swtXulLoader.registerClassLoader( getClass().getClassLoader() );
@@ -90,8 +99,8 @@ public class JobEntryHadoopJobExecutorDialog extends JobEntryDialog implements J
     swtXulLoader.setOuterContext( shell );
 
     container =
-        swtXulLoader.loadXul( "org/pentaho/di/ui/job/entries/hadoopjobexecutor/JobEntryHadoopJobExecutorDialog.xul",
-            bundle ); //$NON-NLS-1$
+      swtXulLoader.loadXul( "org/pentaho/big/data/kettle/plugins/mapreduce/JobEntryHadoopJobExecutorDialog.xul",
+        bundle ); //$NON-NLS-1$
 
     final XulRunner runner = new SwtXulRunner();
     runner.addContainer( container );
@@ -102,60 +111,69 @@ public class JobEntryHadoopJobExecutorDialog extends JobEntryDialog implements J
     bf.setDocument( container.getDocumentRoot() );
     bf.setBindingType( Type.BI_DIRECTIONAL );
 
-    bf.createBinding( "jobentry-name", "value", controller, JobEntryHadoopJobExecutorController.JOB_ENTRY_NAME ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    bf.createBinding( "jobentry-name", "value", controller,
+      JobEntryHadoopJobExecutorController.JOB_ENTRY_NAME ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
     bf.createBinding( "jobentry-hadoopjob-name", "value", controller,
-        JobEntryHadoopJobExecutorController.HADOOP_JOB_NAME ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    bf.createBinding( "jar-url", "value", controller, JobEntryHadoopJobExecutorController.JAR_URL ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    bf.createBinding( "driver-class", "value", controller, JobEntryHadoopJobExecutorController.DRIVER_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    bf.createBinding( "driver-class", "selectedItem", controller, JobEntryHadoopJobExecutorController.DRIVER_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    bf.createBinding( "driver-class", "elements", controller, JobEntryHadoopJobExecutorController.DRIVER_CLASSES ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+      JobEntryHadoopJobExecutorController.HADOOP_JOB_NAME ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    bf.createBinding( "jar-url", "value", controller,
+      JobEntryHadoopJobExecutorController.JAR_URL ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    bf.createBinding( "driver-class", "value", controller,
+      JobEntryHadoopJobExecutorController.DRIVER_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    bf.createBinding( "driver-class", "selectedItem", controller,
+      JobEntryHadoopJobExecutorController.DRIVER_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    bf.createBinding( "driver-class", "elements", controller,
+      JobEntryHadoopJobExecutorController.DRIVER_CLASSES ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     bf.createBinding( "command-line-arguments", "value", controller.getSimpleConfiguration(),
-        SimpleConfiguration.CMD_LINE_ARGS ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+      SimpleConfiguration.CMD_LINE_ARGS ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
     bf.createBinding( "classes-output-key-class", "value", controller.getAdvancedConfiguration(),
-        AdvancedConfiguration.OUTPUT_KEY_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
+      AdvancedConfiguration.OUTPUT_KEY_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
     bf.createBinding( "classes-output-value-class", "value", controller.getAdvancedConfiguration(),
-        AdvancedConfiguration.OUTPUT_VALUE_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
+      AdvancedConfiguration.OUTPUT_VALUE_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
     bf.createBinding( "classes-mapper-class", "value", controller.getAdvancedConfiguration(),
-        AdvancedConfiguration.MAPPER_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
+      AdvancedConfiguration.MAPPER_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
     bf.createBinding( "classes-combiner-class", "value", controller.getAdvancedConfiguration(),
-        AdvancedConfiguration.COMBINER_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
+      AdvancedConfiguration.COMBINER_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
     bf.createBinding( "classes-reducer-class", "value", controller.getAdvancedConfiguration(),
-        AdvancedConfiguration.REDUCER_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
+      AdvancedConfiguration.REDUCER_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
     bf.createBinding( "classes-input-format", "value", controller.getAdvancedConfiguration(),
-        AdvancedConfiguration.INPUT_FORMAT_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
+      AdvancedConfiguration.INPUT_FORMAT_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
     bf.createBinding( "classes-output-format", "value", controller.getAdvancedConfiguration(),
-        AdvancedConfiguration.OUTPUT_FORMAT_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
+      AdvancedConfiguration.OUTPUT_FORMAT_CLASS ); //$NON-NLS-1$ //$NON-NLS-2$
 
     // bf.createBinding("num-map-tasks", "value", controller.getAdvancedConfiguration(),
     // AdvancedConfiguration.NUM_MAP_TASKS, bindingConverter); //$NON-NLS-1$ //$NON-NLS-2$
     bf.createBinding( "num-map-tasks", "value", controller.getAdvancedConfiguration(),
-        AdvancedConfiguration.NUM_MAP_TASKS ); //$NON-NLS-1$ //$NON-NLS-2$
+      AdvancedConfiguration.NUM_MAP_TASKS ); //$NON-NLS-1$ //$NON-NLS-2$
     // bf.createBinding("num-reduce-tasks", "value", controller.getAdvancedConfiguration(),
     // AdvancedConfiguration.NUM_REDUCE_TASKS, bindingConverter); //$NON-NLS-1$ //$NON-NLS-2$
     bf.createBinding( "num-reduce-tasks", "value", controller.getAdvancedConfiguration(),
-        AdvancedConfiguration.NUM_REDUCE_TASKS ); //$NON-NLS-1$ //$NON-NLS-2$
+      AdvancedConfiguration.NUM_REDUCE_TASKS ); //$NON-NLS-1$ //$NON-NLS-2$
 
-    bf.createBinding( "simple-blocking", "selected", controller.getSimpleConfiguration(), SimpleConfiguration.BLOCKING ); //$NON-NLS-1$ //$NON-NLS-2$
-    bf.createBinding( "blocking", "selected", controller.getAdvancedConfiguration(), AdvancedConfiguration.BLOCKING ); //$NON-NLS-1$ //$NON-NLS-2$
+    bf.createBinding( "simple-blocking", "selected", controller.getSimpleConfiguration(),
+      SimpleConfiguration.BLOCKING ); //$NON-NLS-1$ //$NON-NLS-2$
+    bf.createBinding( "blocking", "selected", controller.getAdvancedConfiguration(),
+      AdvancedConfiguration.BLOCKING ); //$NON-NLS-1$ //$NON-NLS-2$
     bf.createBinding( "simple-logging-interval", "value", controller.getSimpleConfiguration(),
-        SimpleConfiguration.LOGGING_INTERVAL ); //$NON-NLS-1$ //$NON-NLS-2$
+      SimpleConfiguration.LOGGING_INTERVAL ); //$NON-NLS-1$ //$NON-NLS-2$
     bf.createBinding( "logging-interval", "value", controller.getAdvancedConfiguration(),
-        AdvancedConfiguration.LOGGING_INTERVAL ); //$NON-NLS-1$ //$NON-NLS-2$
-    bf.createBinding( "input-path", "value", controller.getAdvancedConfiguration(), AdvancedConfiguration.INPUT_PATH ); //$NON-NLS-1$ //$NON-NLS-2$
-    bf.createBinding( "output-path", "value", controller.getAdvancedConfiguration(), AdvancedConfiguration.OUTPUT_PATH ); //$NON-NLS-1$ //$NON-NLS-2$
+      AdvancedConfiguration.LOGGING_INTERVAL ); //$NON-NLS-1$ //$NON-NLS-2$
+    bf.createBinding( "input-path", "value", controller.getAdvancedConfiguration(),
+      AdvancedConfiguration.INPUT_PATH ); //$NON-NLS-1$ //$NON-NLS-2$
+    bf.createBinding( "output-path", "value", controller.getAdvancedConfiguration(),
+      AdvancedConfiguration.OUTPUT_PATH ); //$NON-NLS-1$ //$NON-NLS-2$
 
     ( (XulRadio) container.getDocumentRoot().getElementById( "simpleRadioButton" ) ).setSelected( this.jobEntry
-        .isSimple() ); //$NON-NLS-1$
+      .isSimple() ); //$NON-NLS-1$
     ( (XulRadio) container.getDocumentRoot().getElementById( "advancedRadioButton" ) ).setSelected( !this.jobEntry
-        .isSimple() ); //$NON-NLS-1$
+      .isSimple() ); //$NON-NLS-1$
 
     ( (XulVbox) container.getDocumentRoot().getElementById( "advanced-configuration" ) ).setVisible( !this.jobEntry
-        .isSimple() ); //$NON-NLS-1$
+      .isSimple() ); //$NON-NLS-1$
 
     XulTextbox simpleLoggingInterval =
-        (XulTextbox) container.getDocumentRoot().getElementById( "simple-logging-interval" );
+      (XulTextbox) container.getDocumentRoot().getElementById( "simple-logging-interval" );
     simpleLoggingInterval.setValue( "" + controller.getSimpleConfiguration().getSimpleLoggingInterval() );
 
     XulTextbox loggingInterval = (XulTextbox) container.getDocumentRoot().getElementById( "logging-interval" );
@@ -176,34 +194,40 @@ public class JobEntryHadoopJobExecutorDialog extends JobEntryDialog implements J
     controller.setJobEntry( (JobEntryHadoopJobExecutor) jobEntry );
     controller.init();
 
+    bf.setBindingType( Type.ONE_WAY );
     bf.createBinding( controller, "namedClusters", "named-clusters", "elements" ).fireSourceChanged();
-    bf.createBinding( "named-clusters", "selectedIndex", controller.getAdvancedConfiguration(), "selectedNamedCluster", new BindingConvertor<Integer, NamedCluster>() {
-      public NamedCluster sourceToTarget( final Integer index ) {
-        List<NamedCluster> clusters = controller.getNamedClusters();
-        if ( index == -1 || clusters.isEmpty() ) {
+    bf.setBindingType( Type.BI_DIRECTIONAL );
+    bf.createBinding( "named-clusters", "selectedIndex", controller.getAdvancedConfiguration(), "selectedNamedCluster",
+      new BindingConvertor<Integer, NamedCluster>() {
+        public NamedCluster sourceToTarget( final Integer index ) {
+          List<NamedCluster> clusters = new ArrayList<>();
+          try {
+            clusters = controller.getNamedClusters();
+          } catch ( MetaStoreException e ) {
+            logger.error( e.getMessage(), e );
+          }
+          if ( index == -1 || clusters.isEmpty() ) {
+            return null;
+          }
+          return clusters.get( index );
+        }
+
+        public Integer targetToSource( final NamedCluster value ) {
           return null;
         }
-        return clusters.get( index );
-      }
-
-      public Integer targetToSource( final NamedCluster value ) {
-        return null;
-      }
-    } ).fireSourceChanged();
+      } ).fireSourceChanged();
 
     selectNamedCluster();
   }
 
   private void selectNamedCluster() {
     @SuppressWarnings( "unchecked" )
-    XulMenuList<NamedCluster> namedClusterMenu = (XulMenuList<NamedCluster>) container.getDocumentRoot().getElementById( "named-clusters" ); //$NON-NLS-1$
-    for ( NamedCluster nc : controller.getNamedClusters() ) {
-      String cn = this.jobEntry.getClusterName();
-      if ( cn != null && cn.equals( nc.getName() ) ) {
-        namedClusterMenu.setSelectedItem( nc );
-        controller.getAdvancedConfiguration().setSelectedNamedCluster( nc );
-      }
-    }
+    XulMenuList<NamedCluster> namedClusterMenu =
+      (XulMenuList<NamedCluster>) container.getDocumentRoot().getElementById( "named-clusters" ); //$NON-NLS-1$
+
+    NamedCluster namedCluster = jobEntry.getNamedCluster();
+    namedClusterMenu.setSelectedItem( namedCluster );
+    controller.getAdvancedConfiguration().setSelectedNamedCluster( namedCluster );
   }
 
   public JobEntryInterface open() {

api/mapreduce/pom.xml
@@ -1,0 +1,71 @@
+<?xml version="1.0"?>
+<project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns="http://maven.apache.org/POM/4.0.0"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <modelVersion>4.0.0</modelVersion>
+  <parent>
+    <groupId>pentaho</groupId>
+    <artifactId>pentaho-big-data-api</artifactId>
+    <version>6.1-SNAPSHOT</version>
+  </parent>
+  <artifactId>pentaho-big-data-api-mapreduce</artifactId>
+  <version>6.1-SNAPSHOT</version>
+  <packaging>bundle</packaging>
+  <name>Pentaho Community Edition Project: ${project.artifactId}</name>
+  <description>a Pentaho open source project</description>
+  <url>http://www.pentaho.com</url>
+  <properties>
+    <publish-sonar-phase>site</publish-sonar-phase>
+  </properties>
+  <dependencies>
+    <dependency>
+      <groupId>pentaho</groupId>
+      <artifactId>pentaho-big-data-api-cluster</artifactId>
+      <version>${project.version}</version>
+    </dependency>
+    <dependency>
+      <groupId>pentaho</groupId>
+      <artifactId>pentaho-big-data-api-initializer</artifactId>
+      <version>${project.version}</version>
+    </dependency>
+    <dependency>
+      <groupId>pentaho</groupId>
+      <artifactId>metastore</artifactId>
+      <version>${dependency.metastore.revision}</version>
+      <scope>provided</scope>
+    </dependency>
+    <dependency>
+      <groupId>pentaho-kettle</groupId>
+      <artifactId>kettle-core</artifactId>
+      <version>${dependency.kettle.revision}</version>
+      <scope>provided</scope>
+    </dependency>
+    <dependency>
+      <groupId>junit</groupId>
+      <artifactId>junit</artifactId>
+      <version>${dependency.junit.revision}</version>
+      <scope>test</scope>
+    </dependency>
+    <dependency>
+      <groupId>org.mockito</groupId>
+      <artifactId>mockito-all</artifactId>
+      <version>${dependency.mockito.revision}</version>
+      <scope>test</scope>
+    </dependency>
+  </dependencies>
+  <build>
+    <plugins>
+      <plugin>
+        <groupId>org.apache.felix</groupId>
+        <artifactId>maven-bundle-plugin</artifactId>
+        <version>${dependency.maven-bundle-plugin.revision}</version>
+        <extensions>true</extensions>
+        <configuration>
+          <instructions>
+            <Bundle-SymbolicName>${project.artifactId}</Bundle-SymbolicName>
+            <!--<Export-Package>org.pentaho.bigdata.api.pig,org.pentaho.bigdata.api.pig.impl</Export-Package>-->
+          </instructions>
+        </configuration>
+      </plugin>
+    </plugins>
+  </build>
+</project>

api/mapreduce/src/main/java/org/pentaho/bigdata/api/mapreduce/MapReduceExecutionException.java
@@ -20,41 +20,17 @@
  *
  ******************************************************************************/
 
-package org.pentaho.di.bigdata;
+package org.pentaho.bigdata.api.mapreduce;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Documented
-@Retention( RetentionPolicy.RUNTIME )
-@Target( ElementType.TYPE )
 /**
- * @deprecated Use OSGi plugins instead to access shim functionality
+ * Created by bryan on 12/2/15.
  */
-@Deprecated()
-public @interface ShimDependentJobEntry {
-  String id();
+public class MapReduceExecutionException extends Exception {
+  public MapReduceExecutionException( Throwable cause ) {
+    super( cause );
+  }
 
-  String name() default "";
-
-  String description() default "";
-
-  String image();
-
-  String version() default "";
-
-  int category() default -1;
-
-  String categoryDescription() default "";
-
-  String i18nPackageName() default "";
-
-  String documentationUrl() default "";
-
-  String casesUrl() default "";
-
-  String forumUrl() default "";
+  public MapReduceExecutionException( String message ) {
+    super( message );
+  }
 }

api/mapreduce/src/main/java/org/pentaho/bigdata/api/mapreduce/MapReduceJarInfo.java
@@ -20,41 +20,25 @@
  *
  ******************************************************************************/
 
-package org.pentaho.di.bigdata;
+package org.pentaho.bigdata.api.mapreduce;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.util.List;
 
-@Documented
-@Retention( RetentionPolicy.RUNTIME )
-@Target( ElementType.TYPE )
 /**
- * @deprecated Use OSGi plugins instead to access shim functionality
+ * Created by bryan on 12/4/15.
  */
-@Deprecated()
-public @interface ShimDependentJobEntry {
-  String id();
+public interface MapReduceJarInfo {
+  /**
+   * Returns a list of classes in the jar that have a method with the signature: public static void main(String[] args)
+   *
+   * @return a list of classes in the jar that have a method with the signature: public static void main(String[] args)
+   */
+  List<String> getClassesWithMain();
 
-  String name() default "";
-
-  String description() default "";
-
-  String image();
-
-  String version() default "";
-
-  int category() default -1;
-
-  String categoryDescription() default "";
-
-  String i18nPackageName() default "";
-
-  String documentationUrl() default "";
-
-  String casesUrl() default "";
-
-  String forumUrl() default "";
+  /**
+   * Returns the main class as listed in the manifest of the jar
+   *
+   * @return the main class as listed in the manifest of the jar
+   */
+  String getMainClass();
 }

api/mapreduce/src/main/java/org/pentaho/bigdata/api/mapreduce/MapReduceJob.java
@@ -1,0 +1,68 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.bigdata.api.mapreduce;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Common interface for a running MapReduce job.
+ */
+public interface MapReduceJob {
+  /**
+   * Kills the job
+   *
+   * @throws IOException
+   */
+  void killJob() throws IOException;
+
+  /**
+   * Wait for up to timeout time units (ex: 10 seconds) for the job to complete.
+   *
+   * @param timeout   timeout value
+   * @param timeUnit  the time units of the timeout value
+   * @param stoppable boolean callback taht can terminate the wait early (optional in implementations)
+   * @return a boolean indicating whether the job completed during the wait
+   * @throws IOException
+   * @throws InterruptedException
+   * @throws MapReduceExecutionException
+   */
+  boolean waitOnCompletion( long timeout, TimeUnit timeUnit, MapReduceService.Stoppable stoppable )
+    throws IOException, InterruptedException, MapReduceExecutionException;
+
+  /**
+   * Returns a boolean indicating whether the MapReduce job was successful
+   *
+   * @return a boolean indicating whether the MapReduce job was successful
+   * @throws IOException
+   */
+  boolean isSuccessful() throws IOException;
+
+  /**
+   * Returns a boolean indicating whether the MapReduce job is complete
+   *
+   * @return a boolean indicating whether the MapReduce job is complete
+   * @throws IOException
+   */
+  boolean isComplete() throws IOException;
+}

api/mapreduce/src/main/java/org/pentaho/bigdata/api/mapreduce/MapReduceJobAdvanced.java
@@ -1,0 +1,72 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.bigdata.api.mapreduce;
+
+import java.io.IOException;
+
+/**
+ * MapReduce job interface that supports progress monitoring, completion events, and task diagnostics
+ */
+public interface MapReduceJobAdvanced extends MapReduceJob {
+  /**
+   * Returns the setup progress
+   *
+   * @return the setup progress
+   * @throws IOException
+   */
+  double getSetupProgress() throws IOException;
+
+  /**
+   * Returns the map progress
+   *
+   * @return the map progress
+   * @throws IOException
+   */
+  double getMapProgress() throws IOException;
+
+  /**
+   * Returns the reduce progress
+   *
+   * @return the reduce progress
+   * @throws IOException
+   */
+  double getReduceProgress() throws IOException;
+
+  /**
+   * Returns the TaskCompletionEvents starting at the given start index
+   *
+   * @param startIndex the start index
+   * @return the TaskCompletionEvents starting at the given start index
+   * @throws IOException
+   */
+  TaskCompletionEvent[] getTaskCompletionEvents( int startIndex ) throws IOException;
+
+  /**
+   * Returns the task diagnostics for a given attempt id
+   *
+   * @param taskAttemptId the attempt id
+   * @return the task diagnostics for a given attempt id
+   * @throws IOException
+   */
+  String[] getTaskDiagnostics( Object taskAttemptId ) throws IOException;
+}

commit 0c0d739d309807fc178710dd8261f144071ec90d

[BACKLOG-5908]: REG: Hadoop Job Executer doesn't use correct information from named cluster.
This issue was introduced with changes in the commit: https://github.com/pentaho/big-data-plugin/commit/ceb855aa6b215b43a82f9b5c353527a29b83d3d7
(exactly starting from the line:
https://github.com/pentaho/big-data-plugin/blob/ceb855aa6b215b43a82f9b5c353527a29b83d3d7/kettle-plugins/mapreduce/src/main/java/org/pentaho/big/data/kettle/plugins/mapreduce/JobEntryHadoopJobExecutorDialog.java#L228)

So the fix is just to return the behavior that was before.
No unit test because the change is on UI (in Dialog).

kettle-plugins/mapreduce/src/main/java/org/pentaho/big/data/kettle/plugins/mapreduce/JobEntryHadoopJobExecutorDialog.java
@@ -223,11 +223,13 @@ public class JobEntryHadoopJobExecutorDialog extends JobEntryDialog implements J
   private void selectNamedCluster() {
     @SuppressWarnings( "unchecked" )
     XulMenuList<NamedCluster> namedClusterMenu =
-      (XulMenuList<NamedCluster>) container.getDocumentRoot().getElementById( "named-clusters" ); //$NON-NLS-1$
+        (XulMenuList<NamedCluster>) container.getDocumentRoot().getElementById( "named-clusters" ); //$NON-NLS-1$
 
     NamedCluster namedCluster = jobEntry.getNamedCluster();
-    namedClusterMenu.setSelectedItem( namedCluster );
-    controller.getAdvancedConfiguration().setSelectedNamedCluster( namedCluster );
+    if ( isKnownNamedCluster( namedCluster, controller ) ) {
+      namedClusterMenu.setSelectedItem( namedCluster );
+      controller.getAdvancedConfiguration().setSelectedNamedCluster( namedCluster );
+    }
   }
 
   public JobEntryInterface open() {
@@ -235,6 +237,26 @@ public class JobEntryHadoopJobExecutorDialog extends JobEntryDialog implements J
     dialog.show();
 
     return jobEntry;
+  }
+
+  private boolean isKnownNamedCluster( NamedCluster jobNameCluster, JobEntryHadoopJobExecutorController controller ) {
+    boolean result = false;
+    String jncName = jobNameCluster.getName();
+    List<NamedCluster> nClusters = null;
+    try {
+      nClusters = controller.getNamedClusters();
+    } catch ( MetaStoreException e ) {
+      logger.error( e.getMessage(), e );
+    }
+    if ( jncName != null && nClusters != null ) {
+      for ( NamedCluster nc : nClusters ) {
+        if ( jncName != null && jncName.equals( nc.getName() ) ) {
+          result = true;
+          break;
+        }
+      }
+    }
+    return result;
   }
 
 }