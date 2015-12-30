pentaho/big-data-plugin

commit ceb855aa6b215b43a82f9b5c353527a29b83d3d7

[BACKLOG-4102] - MapReduce OSGi
Api, shim impl, move legacy plugins to kettle-plugins bundle, assembly update

api/mapreduce/src/main/java/org/pentaho/bigdata/api/mapreduce/MapReduceJobBuilder.java
@@ -1,0 +1,137 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.bigdata.api.mapreduce;
+
+import java.net.URL;
+
+/**
+ * Builder interface for configuring MapReduce jobs
+ */
+public interface MapReduceJobBuilder {
+  /**
+   * Sets the url of the jar to run
+   *
+   * @param resolvedJarUrl the url of the jar to run
+   */
+  void setResolvedJarUrl( URL resolvedJarUrl );
+
+  /**
+   * Sets the job name
+   *
+   * @param hadoopJobName the job name
+   */
+  void setHadoopJobName( String hadoopJobName );
+
+  /**
+   * Sets the output key class
+   *
+   * @param outputKeyClass the output key class
+   */
+  void setOutputKeyClass( String outputKeyClass );
+
+  /**
+   * Sets the output value class
+   *
+   * @param outputValueClass the output value class
+   */
+  void setOutputValueClass( String outputValueClass );
+
+  /**
+   * Sets the mapper class
+   *
+   * @param mapperClass the mapper class
+   */
+  void setMapperClass( String mapperClass );
+
+  /**
+   * Sets the combiner class
+   *
+   * @param combinerClass the combiner class
+   */
+  void setCombinerClass( String combinerClass );
+
+  /**
+   * Sets the reducer class
+   *
+   * @param reducerClass the reducer class
+   */
+  void setReducerClass( String reducerClass );
+
+  /**
+   * Sets the input format class
+   *
+   * @param inputFormatClass the input format class
+   */
+  void setInputFormatClass( String inputFormatClass );
+
+  /**
+   * Sets the output format class
+   *
+   * @param outputFormatClass the output format class
+   */
+  void setOutputFormatClass( String outputFormatClass );
+
+  /**
+   * Sets the input path (comma separated list)
+   *
+   * @param inputPath the input path (comma separated list)
+   */
+  void setInputPath( String inputPath );
+
+  /**
+   * Sets the number of map tasks
+   *
+   * @param numMapTasks the number of map tasks
+   */
+  void setNumMapTasks( int numMapTasks );
+
+  /**
+   * Sets the number of reduce tasks
+   *
+   * @param numReduceTasks the number of reduce tasks
+   */
+  void setNumReduceTasks( int numReduceTasks );
+
+  /**
+   * Sets the output path
+   *
+   * @param outputPath the output path
+   */
+  void setOutputPath( String outputPath );
+
+  /**
+   * Puts arbitrary variables in the job configuration
+   *
+   * @param key   the key
+   * @param value the value
+   */
+  void putUserDefined( String key, String value );
+
+  /**
+   * Submits the job to the cluster
+   *
+   * @return a MapReduceJobAdvanced for monitoring job progress
+   * @throws Exception
+   */
+  MapReduceJobAdvanced submit() throws Exception;
+}

impl/shim/mapreduce/src/main/java/org/pentaho/big/data/impl/shim/mapreduce/MapReduceJobBuilderImpl.java
@@ -1,0 +1,200 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.big.data.impl.shim.mapreduce;
+
+import org.pentaho.big.data.api.cluster.NamedCluster;
+import org.pentaho.bigdata.api.mapreduce.MapReduceJobAdvanced;
+import org.pentaho.bigdata.api.mapreduce.MapReduceJobBuilder;
+import org.pentaho.di.core.logging.LogChannelInterface;
+import org.pentaho.di.core.variables.VariableSpace;
+import org.pentaho.hadoop.shim.api.Configuration;
+import org.pentaho.hadoop.shim.api.fs.FileSystem;
+import org.pentaho.hadoop.shim.api.fs.Path;
+import org.pentaho.hadoop.shim.spi.HadoopShim;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by bryan on 12/3/15.
+ */
+public class MapReduceJobBuilderImpl implements MapReduceJobBuilder {
+  private final NamedCluster namedCluster;
+  private final HadoopShim hadoopShim;
+  private final LogChannelInterface log;
+  private final VariableSpace variableSpace;
+  private final Map<String, String> userDefined;
+  private URL resolvedJarUrl;
+  private String hadoopJobName;
+  private String outputKeyClass;
+  private String outputValueClass;
+  private String mapperClass;
+  private String combinerClass;
+  private String reducerClass;
+  private String inputFormatClass;
+  private String outputFormatClass;
+  private String inputPath;
+  private int numMapTasks;
+  private int numReduceTasks;
+  private String outputPath;
+
+  public MapReduceJobBuilderImpl( NamedCluster namedCluster, HadoopShim hadoopShim, LogChannelInterface log,
+                                  VariableSpace variableSpace ) {
+    this.namedCluster = namedCluster;
+    this.hadoopShim = hadoopShim;
+    this.log = log;
+    this.variableSpace = variableSpace;
+    this.userDefined = new HashMap<>();
+  }
+
+  @Override public void setResolvedJarUrl( URL resolvedJarUrl ) {
+    this.resolvedJarUrl = resolvedJarUrl;
+  }
+
+  @Override public void setHadoopJobName( String hadoopJobName ) {
+    this.hadoopJobName = hadoopJobName;
+  }
+
+  @Override public void setOutputKeyClass( String outputKeyClass ) {
+    this.outputKeyClass = outputKeyClass;
+  }
+
+  @Override public void setOutputValueClass( String outputValueClass ) {
+    this.outputValueClass = outputValueClass;
+  }
+
+  @Override public void setMapperClass( String mapperClass ) {
+    this.mapperClass = mapperClass;
+  }
+
+  @Override public void setCombinerClass( String combinerClass ) {
+    this.combinerClass = combinerClass;
+  }
+
+  @Override public void setReducerClass( String reducerClass ) {
+    this.reducerClass = reducerClass;
+  }
+
+  @Override public void setInputFormatClass( String inputFormatClass ) {
+    this.inputFormatClass = inputFormatClass;
+  }
+
+  @Override public void setOutputFormatClass( String outputFormatClass ) {
+    this.outputFormatClass = outputFormatClass;
+  }
+
+  @Override public void setInputPath( String inputPath ) {
+    this.inputPath = inputPath;
+  }
+
+  @Override public void setNumMapTasks( int numMapTasks ) {
+    this.numMapTasks = numMapTasks;
+  }
+
+  @Override public void setNumReduceTasks( int numReduceTasks ) {
+    this.numReduceTasks = numReduceTasks;
+  }
+
+  @Override public void setOutputPath( String outputPath ) {
+    this.outputPath = outputPath;
+  }
+
+  @Override public void putUserDefined( String key, String value ) {
+    userDefined.put( key, value );
+  }
+
+  @Override public MapReduceJobAdvanced submit() throws Exception {
+    Configuration conf = hadoopShim.createConfiguration();
+    FileSystem fs = hadoopShim.getFileSystem( conf );
+    URL[] urls = new URL[] { resolvedJarUrl };
+    URLClassLoader loader = new URLClassLoader( urls, hadoopShim.getClass().getClassLoader() );
+    conf.setJobName( hadoopJobName );
+
+    conf.setOutputKeyClass( loader.loadClass( outputKeyClass ) );
+    conf.setOutputValueClass( loader.loadClass( outputValueClass ) );
+
+    if ( mapperClass != null ) {
+      Class<?> mapper = loader.loadClass( mapperClass );
+      conf.setMapperClass( mapper );
+    }
+    if ( combinerClass != null ) {
+      Class<?> combiner = loader.loadClass( combinerClass );
+      conf.setCombinerClass( combiner );
+    }
+    if ( reducerClass != null ) {
+      Class<?> reducer = loader.loadClass( reducerClass );
+      conf.setReducerClass( reducer );
+    }
+
+    if ( inputFormatClass != null ) {
+      Class<?> inputFormat = loader.loadClass( inputFormatClass );
+      conf.setInputFormat( inputFormat );
+    }
+    if ( outputFormatClass != null ) {
+      Class<?> outputFormat = loader.loadClass( outputFormatClass );
+      conf.setOutputFormat( outputFormat );
+    }
+
+    String hdfsHostnameS = variableSpace.environmentSubstitute( namedCluster.getHdfsHost() );
+    String hdfsPortS = variableSpace.environmentSubstitute( namedCluster.getHdfsPort() );
+    String jobTrackerHostnameS = variableSpace.environmentSubstitute( namedCluster.getJobTrackerHost() );
+    String jobTrackerPortS = variableSpace.environmentSubstitute( namedCluster.getJobTrackerPort() );
+
+    List<String> configMessages = new ArrayList<String>();
+    hadoopShim.configureConnectionInformation( hdfsHostnameS, hdfsPortS, jobTrackerHostnameS, jobTrackerPortS, conf,
+      configMessages );
+    for ( String m : configMessages ) {
+      log.logBasic( m );
+    }
+
+    String[] inputPathParts = inputPath.split( "," );
+    List<Path> paths = new ArrayList<Path>();
+    for ( String path : inputPathParts ) {
+      paths.add( fs.asPath( conf.getDefaultFileSystemURL(), path ) );
+    }
+    Path[] finalPaths = paths.toArray( new Path[ paths.size() ] );
+
+    conf.setInputPaths( finalPaths );
+    conf.setOutputPath( fs.asPath( conf.getDefaultFileSystemURL(), outputPath ) );
+
+    // process user defined values
+    for ( Map.Entry<String, String> stringStringEntry : userDefined.entrySet() ) {
+      String key = stringStringEntry.getKey();
+      String value = stringStringEntry.getValue();
+      if ( key != null && !"".equals( key ) && value != null && !"".equals( value ) ) {
+        conf.set( key, value );
+      }
+    }
+
+    conf.setJar( resolvedJarUrl.toString() );
+
+    conf.setNumMapTasks( numMapTasks );
+    conf.setNumReduceTasks( numReduceTasks );
+
+    return new RunningJobMapReduceJobAdvancedImpl( hadoopShim.submitJob( conf ) );
+  }
+}

impl/shim/mapreduce/src/test/java/org/pentaho/big/data/impl/shim/mapreduce/MapReduceJobBuilderImplTest.java
@@ -1,0 +1,205 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.big.data.impl.shim.mapreduce;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.pentaho.big.data.api.cluster.NamedCluster;
+import org.pentaho.di.core.logging.LogChannelInterface;
+import org.pentaho.di.core.variables.VariableSpace;
+import org.pentaho.hadoop.shim.api.Configuration;
+import org.pentaho.hadoop.shim.api.fs.FileSystem;
+import org.pentaho.hadoop.shim.api.fs.Path;
+import org.pentaho.hadoop.shim.api.mapred.RunningJob;
+import org.pentaho.hadoop.shim.spi.HadoopShim;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isNull;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by bryan on 12/7/15.
+ */
+public class MapReduceJobBuilderImplTest {
+  private NamedCluster namedCluster;
+  private HadoopShim hadoopShim;
+  private LogChannelInterface logChannelInterface;
+  private VariableSpace variableSpace;
+  private MapReduceJobBuilderImpl mapReduceJobBuilder;
+  private Configuration configuration;
+  private FileSystem fileSystem;
+  private String testJobName;
+  private String inputPath;
+  private URL resolvedJarUrl;
+  private int numMapTasks;
+  private int numReduceTasks;
+  private String userPath;
+  private String userPath2;
+  private String fsUrl;
+  private Path userPath1Path;
+  private Path userPath2Path;
+  private RunningJob runningJob;
+  private float magicSetupNumber;
+  private String logMeBasic;
+  private String hdfsHost;
+  private String hdfsPort;
+  private String jobTrackerHost;
+  private String jobTrackerPort;
+
+  @Before
+  public void setup() throws IOException {
+    namedCluster = mock( NamedCluster.class );
+    hadoopShim = mock( HadoopShim.class );
+    logChannelInterface = mock( LogChannelInterface.class );
+    variableSpace = mock( VariableSpace.class );
+    configuration = mock( Configuration.class );
+    fileSystem = mock( FileSystem.class );
+    runningJob = mock( RunningJob.class );
+    magicSetupNumber = 1337f;
+    when( runningJob.setupProgress() ).thenReturn( magicSetupNumber );
+
+    when( hadoopShim.createConfiguration() ).thenReturn( configuration );
+    when( hadoopShim.getFileSystem( configuration ) ).thenReturn( fileSystem );
+    when( hadoopShim.submitJob( configuration ) ).thenReturn( runningJob );
+    fsUrl = "hdfs://";
+    when( configuration.getDefaultFileSystemURL() ).thenReturn( fsUrl );
+
+    hdfsHost = "hdfsHost";
+    hdfsPort = "hdfsPort";
+    jobTrackerHost = "jobTrackerHost";
+    jobTrackerPort = "jobTrackerPort";
+    when( variableSpace.environmentSubstitute( "a" ) ).thenReturn( hdfsHost );
+    when( variableSpace.environmentSubstitute( "b" ) ).thenReturn( hdfsPort );
+    when( variableSpace.environmentSubstitute( "c" ) ).thenReturn( jobTrackerHost );
+    when( variableSpace.environmentSubstitute( "d" ) ).thenReturn( jobTrackerPort );
+    when( namedCluster.getHdfsHost() ).thenReturn( "a" );
+    when( namedCluster.getHdfsPort() ).thenReturn( "b" );
+    when( namedCluster.getJobTrackerHost() ).thenReturn( "c" );
+    when( namedCluster.getJobTrackerPort() ).thenReturn( "d" );
+
+    mapReduceJobBuilder = new MapReduceJobBuilderImpl( namedCluster, hadoopShim, logChannelInterface, variableSpace );
+
+    testJobName = "testJobName";
+    userPath = "/user/path";
+    userPath1Path = mock( Path.class );
+    when( fileSystem.asPath( fsUrl, userPath ) ).thenReturn( userPath1Path );
+    userPath2 = "/user/path2";
+    userPath2Path = mock( Path.class );
+    when( fileSystem.asPath( fsUrl, userPath2 ) ).thenReturn( userPath2Path );
+    inputPath = userPath + "," + userPath2;
+    resolvedJarUrl = new URL( "http://jar.com/myjar" );
+    numMapTasks = 3;
+    numReduceTasks = 1;
+    logMeBasic = "log me basic";
+  }
+
+  @Test
+  public void testMinimal() throws Exception {
+    mapReduceJobBuilder.setHadoopJobName( testJobName );
+    mapReduceJobBuilder.setOutputKeyClass( String.class.getCanonicalName() );
+    mapReduceJobBuilder.setOutputValueClass( String.class.getCanonicalName() );
+    mapReduceJobBuilder.setInputPath( inputPath );
+    mapReduceJobBuilder.setResolvedJarUrl( resolvedJarUrl );
+    mapReduceJobBuilder.setNumMapTasks( numMapTasks );
+    mapReduceJobBuilder.setNumReduceTasks( numReduceTasks );
+
+    assertEquals( magicSetupNumber, mapReduceJobBuilder.submit().getSetupProgress(), 0 );
+
+    verify( configuration ).setJobName( testJobName );
+    verify( configuration ).setOutputKeyClass( String.class );
+    verify( configuration ).setOutputValueClass( String.class );
+    verify( configuration ).setInputPaths( userPath1Path, userPath2Path );
+    verify( configuration ).setJar( resolvedJarUrl.toString() );
+    verify( configuration ).setNumMapTasks( numMapTasks );
+    verify( configuration ).setNumReduceTasks( numReduceTasks );
+  }
+
+  @Test
+  public void testMaxConfig() throws Exception {
+    mapReduceJobBuilder.setHadoopJobName( testJobName );
+    mapReduceJobBuilder.setOutputKeyClass( String.class.getCanonicalName() );
+    mapReduceJobBuilder.setOutputValueClass( String.class.getCanonicalName() );
+    mapReduceJobBuilder.setMapperClass( Map.class.getCanonicalName() );
+    mapReduceJobBuilder.setCombinerClass( List.class.getCanonicalName() );
+    mapReduceJobBuilder.setReducerClass( Set.class.getCanonicalName() );
+    mapReduceJobBuilder.setInputFormatClass( Integer.class.getCanonicalName() );
+    mapReduceJobBuilder.setOutputFormatClass( Float.class.getCanonicalName() );
+    mapReduceJobBuilder.setInputPath( inputPath );
+    mapReduceJobBuilder.setOutputPath( userPath2 );
+    mapReduceJobBuilder.setResolvedJarUrl( resolvedJarUrl );
+    mapReduceJobBuilder.setNumMapTasks( numMapTasks );
+    mapReduceJobBuilder.setNumReduceTasks( numReduceTasks );
+    String defA = "defA";
+    String valA = "valA";
+    mapReduceJobBuilder.putUserDefined( defA, valA );
+    mapReduceJobBuilder.putUserDefined( null, "valB" );
+    mapReduceJobBuilder.putUserDefined( "", "valB" );
+    mapReduceJobBuilder.putUserDefined( "valC", null );
+    mapReduceJobBuilder.putUserDefined( "valB", "" );
+
+    doAnswer( new Answer<Void>() {
+      @Override public Void answer( InvocationOnMock invocation ) throws Throwable {
+        List<String> msgs = (List<String>) invocation.getArguments()[ 5 ];
+        msgs.add( logMeBasic );
+        return null;
+      }
+    } ).when( hadoopShim ).configureConnectionInformation( eq( hdfsHost ), eq( hdfsPort ), eq( jobTrackerHost ),
+      eq( jobTrackerPort ), eq( configuration ), eq( new ArrayList<String>() ) );
+    assertEquals( magicSetupNumber, mapReduceJobBuilder.submit().getSetupProgress(), 0 );
+
+    verify( configuration ).setJobName( testJobName );
+    verify( configuration ).setOutputKeyClass( String.class );
+    verify( configuration ).setOutputValueClass( String.class );
+    verify( configuration ).setMapperClass( Map.class );
+    verify( configuration ).setCombinerClass( List.class );
+    verify( configuration ).setReducerClass( Set.class );
+    verify( configuration ).setInputFormat( Integer.class );
+    verify( configuration ).setOutputFormat( Float.class );
+    verify( configuration ).setInputPaths( userPath1Path, userPath2Path );
+    verify( configuration ).setOutputPath( userPath2Path );
+    verify( configuration ).setJar( resolvedJarUrl.toString() );
+    verify( configuration ).setNumMapTasks( numMapTasks );
+    verify( configuration ).setNumReduceTasks( numReduceTasks );
+    verify( logChannelInterface ).logBasic( logMeBasic );
+    verify( configuration ).set( defA, valA );
+    verify( configuration, never() ).set( isNull( String.class ), anyString() );
+    verify( configuration, never() ).set( eq( "" ), anyString() );
+    verify( configuration, never() ).set( eq( "valB" ), isNull( String.class ) );
+    verify( configuration, never() ).set( eq( "valB" ), eq( "" ) );
+  }
+}

api/mapreduce/pom.xml
@@ -1,0 +1,71 @@
+<?xml version="1.0"?>
+<project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns="http://maven.apache.org/POM/4.0.0"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <modelVersion>4.0.0</modelVersion>
+  <parent>
+    <groupId>pentaho</groupId>
+    <artifactId>pentaho-big-data-api</artifactId>
+    <version>6.1-SNAPSHOT</version>
+  </parent>
+  <artifactId>pentaho-big-data-api-mapreduce</artifactId>
+  <version>6.1-SNAPSHOT</version>
+  <packaging>bundle</packaging>
+  <name>Pentaho Community Edition Project: ${project.artifactId}</name>
+  <description>a Pentaho open source project</description>
+  <url>http://www.pentaho.com</url>
+  <properties>
+    <publish-sonar-phase>site</publish-sonar-phase>
+  </properties>
+  <dependencies>
+    <dependency>
+      <groupId>pentaho</groupId>
+      <artifactId>pentaho-big-data-api-cluster</artifactId>
+      <version>${project.version}</version>
+    </dependency>
+    <dependency>
+      <groupId>pentaho</groupId>
+      <artifactId>pentaho-big-data-api-initializer</artifactId>
+      <version>${project.version}</version>
+    </dependency>
+    <dependency>
+      <groupId>pentaho</groupId>
+      <artifactId>metastore</artifactId>
+      <version>${dependency.metastore.revision}</version>
+      <scope>provided</scope>
+    </dependency>
+    <dependency>
+      <groupId>pentaho-kettle</groupId>
+      <artifactId>kettle-core</artifactId>
+      <version>${dependency.kettle.revision}</version>
+      <scope>provided</scope>
+    </dependency>
+    <dependency>
+      <groupId>junit</groupId>
+      <artifactId>junit</artifactId>
+      <version>${dependency.junit.revision}</version>
+      <scope>test</scope>
+    </dependency>
+    <dependency>
+      <groupId>org.mockito</groupId>
+      <artifactId>mockito-all</artifactId>
+      <version>${dependency.mockito.revision}</version>
+      <scope>test</scope>
+    </dependency>
+  </dependencies>
+  <build>
+    <plugins>
+      <plugin>
+        <groupId>org.apache.felix</groupId>
+        <artifactId>maven-bundle-plugin</artifactId>
+        <version>${dependency.maven-bundle-plugin.revision}</version>
+        <extensions>true</extensions>
+        <configuration>
+          <instructions>
+            <Bundle-SymbolicName>${project.artifactId}</Bundle-SymbolicName>
+            <!--<Export-Package>org.pentaho.bigdata.api.pig,org.pentaho.bigdata.api.pig.impl</Export-Package>-->
+          </instructions>
+        </configuration>
+      </plugin>
+    </plugins>
+  </build>
+</project>

commit af4fdb1c2a8cb371b936f65f2f3c82c5b2a8090c

[BACKLOG-5885]: REG: Hadoop Job Executor: Error in case "Jar" field contains spaces in path
The issue was introduced with the commit: https://github.com/pentaho/big-data-plugin/commit/ceb855aa6b215b43a82f9b5c353527a29b83d3d7
On the line: https://github.com/pentaho/big-data-plugin/blob/ceb855aa6b215b43a82f9b5c353527a29b83d3d7/impl/shim/mapreduce/src/main/java/org/pentaho/big/data/impl/shim/mapreduce/MapReduceJobBuilderImpl.java#L193
we set into hadoop config.jar the path string after processing in resolveJarUrl method.

For the case when jarUrl has a space character (e.g. D:\PENT\ISSUES\6.1\BACKLOG-5885\Folder Test\pentaho-mapreduce-sample_MR2.jar),
we will set config.jar=file:/D:/PENT/ISSUES/6.1/BACKLOG-5885/Folder%20Test/pentaho-mapreduce-sample_MR2.jar with already encoded space.
Hadoop during processing jars (org.apache.hadoop.mapreduce.JobSubmitter.copyAndConfigureFiles(Job job, Path submitJobDir, short replication)) constructs its own path to jar (org.apache.hadoop.fs.Path jobJarPath = new org.apache.hadoop.fs.Path(jobJar))
And it seems that hadoop escapes %20 one more time (it looks like Folder%2520Test). In any case according to the doc (https://hadoop.apache.org/docs/r2.6.2/api/org/apache/hadoop/fs/Path.html#Path%28java.lang.String%29)
we should use path strings that are URIs, but with unescaped elements.

So the fix is just to use exactly that path that was entered by the user in config.jar  without any additional processing as it was implemented before.
Unit tests have been corrected.

api/mapreduce/src/main/java/org/pentaho/bigdata/api/mapreduce/MapReduceJobBuilder.java
@@ -36,6 +36,13 @@ public interface MapReduceJobBuilder {
   void setResolvedJarUrl( URL resolvedJarUrl );
 
   /**
+   * Sets the jar to run
+   *
+   * @param jarUrl the jar to run
+   */
+  void setJarUrl( String jarUrl );
+
+  /**
    * Sets the job name
    *
    * @param hadoopJobName the job name

impl/shim/mapreduce/src/main/java/org/pentaho/big/data/impl/shim/mapreduce/MapReduceJobBuilderImpl.java
@@ -49,6 +49,7 @@ public class MapReduceJobBuilderImpl implements MapReduceJobBuilder {
   private final VariableSpace variableSpace;
   private final Map<String, String> userDefined;
   private URL resolvedJarUrl;
+  private String jarUrl;
   private String hadoopJobName;
   private String outputKeyClass;
   private String outputValueClass;
@@ -73,6 +74,11 @@ public class MapReduceJobBuilderImpl implements MapReduceJobBuilder {
 
   @Override public void setResolvedJarUrl( URL resolvedJarUrl ) {
     this.resolvedJarUrl = resolvedJarUrl;
+  }
+
+  @Override
+  public void setJarUrl( String jarUrl ) {
+    this.jarUrl = jarUrl;
   }
 
   @Override public void setHadoopJobName( String hadoopJobName ) {
@@ -190,7 +196,7 @@ public class MapReduceJobBuilderImpl implements MapReduceJobBuilder {
       }
     }
 
-    conf.setJar( resolvedJarUrl.toString() );
+    conf.setJar( jarUrl );
 
     conf.setNumMapTasks( numMapTasks );
     conf.setNumReduceTasks( numReduceTasks );

kettle-plugins/mapreduce/src/main/java/org/pentaho/big/data/kettle/plugins/mapreduce/JobEntryHadoopJobExecutor.java
@@ -366,6 +366,7 @@ public class JobEntryHadoopJobExecutor extends JobEntryBase implements Cloneable
         MapReduceJobBuilder jobBuilder = mapReduceService.createJobBuilder( log, variables );
 
         jobBuilder.setResolvedJarUrl( resolvedJarUrl );
+        jobBuilder.setJarUrl( environmentSubstitute( jarUrl ) );
         jobBuilder.setHadoopJobName( environmentSubstitute( hadoopJobName ) );
 
         jobBuilder.setOutputKeyClass( environmentSubstitute( outputKeyClass ) );

impl/shim/mapreduce/src/test/java/org/pentaho/big/data/impl/shim/mapreduce/MapReduceJobBuilderImplTest.java
@@ -66,6 +66,7 @@ public class MapReduceJobBuilderImplTest {
   private String testJobName;
   private String inputPath;
   private URL resolvedJarUrl;
+  private String jarUrl;
   private int numMapTasks;
   private int numReduceTasks;
   private String userPath;
@@ -122,6 +123,7 @@ public class MapReduceJobBuilderImplTest {
     userPath2Path = mock( Path.class );
     when( fileSystem.asPath( fsUrl, userPath2 ) ).thenReturn( userPath2Path );
     inputPath = userPath + "," + userPath2;
+    jarUrl = "http://jar.com/myjar";
     resolvedJarUrl = new URL( "http://jar.com/myjar" );
     numMapTasks = 3;
     numReduceTasks = 1;
@@ -134,6 +136,7 @@ public class MapReduceJobBuilderImplTest {
     mapReduceJobBuilder.setOutputKeyClass( String.class.getCanonicalName() );
     mapReduceJobBuilder.setOutputValueClass( String.class.getCanonicalName() );
     mapReduceJobBuilder.setInputPath( inputPath );
+    mapReduceJobBuilder.setJarUrl( jarUrl );
     mapReduceJobBuilder.setResolvedJarUrl( resolvedJarUrl );
     mapReduceJobBuilder.setNumMapTasks( numMapTasks );
     mapReduceJobBuilder.setNumReduceTasks( numReduceTasks );
@@ -144,7 +147,7 @@ public class MapReduceJobBuilderImplTest {
     verify( configuration ).setOutputKeyClass( String.class );
     verify( configuration ).setOutputValueClass( String.class );
     verify( configuration ).setInputPaths( userPath1Path, userPath2Path );
-    verify( configuration ).setJar( resolvedJarUrl.toString() );
+    verify( configuration ).setJar( jarUrl );
     verify( configuration ).setNumMapTasks( numMapTasks );
     verify( configuration ).setNumReduceTasks( numReduceTasks );
   }
@@ -161,6 +164,7 @@ public class MapReduceJobBuilderImplTest {
     mapReduceJobBuilder.setOutputFormatClass( Float.class.getCanonicalName() );
     mapReduceJobBuilder.setInputPath( inputPath );
     mapReduceJobBuilder.setOutputPath( userPath2 );
+    mapReduceJobBuilder.setJarUrl( jarUrl );
     mapReduceJobBuilder.setResolvedJarUrl( resolvedJarUrl );
     mapReduceJobBuilder.setNumMapTasks( numMapTasks );
     mapReduceJobBuilder.setNumReduceTasks( numReduceTasks );
@@ -192,7 +196,7 @@ public class MapReduceJobBuilderImplTest {
     verify( configuration ).setOutputFormat( Float.class );
     verify( configuration ).setInputPaths( userPath1Path, userPath2Path );
     verify( configuration ).setOutputPath( userPath2Path );
-    verify( configuration ).setJar( resolvedJarUrl.toString() );
+    verify( configuration ).setJar( jarUrl );
     verify( configuration ).setNumMapTasks( numMapTasks );
     verify( configuration ).setNumReduceTasks( numReduceTasks );
     verify( logChannelInterface ).logBasic( logMeBasic );