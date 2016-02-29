67fa5a24cc6607373bdd2a56f69ce89b4041a515
brosander <brosander@pentaho.com>
2016-02-29 11:39:22 -0500

    [TEST] - More HBase tests

--- impl/shim/hbase/src/main/java/com/pentaho/big/data/bundles/impl/shim/hbase/table/ResultScannerImpl.java
+++ impl/shim/hbase/src/main/java/com/pentaho/big/data/bundles/impl/shim/hbase/table/ResultScannerImpl.java
@@ -25,7 +25,6 @@ package com.pentaho.big.data.bundles.impl.shim.hbase.table;
 import com.pentaho.big.data.bundles.impl.shim.hbase.HBaseConnectionWrapper;
 import com.pentaho.big.data.bundles.impl.shim.hbase.ResultImpl;
 import com.pentaho.big.data.bundles.impl.shim.hbase.connectionPool.HBaseConnectionHandle;
-import org.pentaho.bigdata.api.hbase.Result;
 import org.pentaho.bigdata.api.hbase.table.ResultScanner;
 import org.pentaho.hbase.shim.spi.HBaseBytesUtilShim;
 
@@ -45,7 +44,7 @@ public class ResultScannerImpl implements ResultScanner {
     hBaseConnectionWrapper = hBaseConnectionHandle.getConnection();
   }
 
-  @Override public Result next() throws IOException {
+  @Override public ResultImpl next() throws IOException {
     try {
       if ( !hBaseConnectionWrapper.resultSetNextRow() ) {
         return null;
