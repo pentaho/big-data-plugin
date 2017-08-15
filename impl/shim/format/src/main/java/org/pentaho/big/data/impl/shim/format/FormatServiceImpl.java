9e26ea556fdde358afb09eb32041ea199c407007
Alexander Buloichik <alexander_buloichik@epam.com>
2017-08-15 11:07:24 +0300

    [BACKLOG-17880] Add format shims implementation

---
+++ impl/shim/format/src/main/java/org/pentaho/big/data/impl/shim/format/FormatServiceImpl.java
@@ -0,0 +1,60 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2017 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+package org.pentaho.big.data.impl.shim.format;
+
+import org.pentaho.big.data.api.cluster.NamedCluster;
+import org.pentaho.bigdata.api.format.FormatService;
+import org.pentaho.hadoop.shim.ConfigurationException;
+import org.pentaho.hadoop.shim.HadoopConfiguration;
+import org.pentaho.hadoop.shim.api.Configuration;
+import org.pentaho.hadoop.shim.api.format.PentahoInputFormat;
+import org.pentaho.hadoop.shim.api.format.PentahoOutputFormat;
+import org.pentaho.hadoop.shim.api.format.SchemaDescription;
+import org.pentaho.hadoop.shim.spi.FormatShim;
+
+public class FormatServiceImpl implements FormatService {
+
+  private final FormatShim formatShim;
+  private final NamedCluster namedCluster;
+  private final HadoopConfiguration hadoopConfiguration;
+
+  public FormatServiceImpl( NamedCluster namedCluster, HadoopConfiguration hadoopConfiguration )
+    throws ConfigurationException {
+    this.namedCluster = namedCluster;
+    this.hadoopConfiguration = hadoopConfiguration;
+    this.formatShim = hadoopConfiguration.getFormatShim();
+  }
+
+  @Override public PentahoInputFormat getInputFormat( Configuration configuration, SchemaDescription schemaDescription ) {
+    return formatShim.createInputFormat( FormatShim.FormatType.PARQUET, configuration, schemaDescription );
+  }
+
+  @Override
+  public PentahoOutputFormat getOutputFormat( Configuration configuration, SchemaDescription schemaDescription ) {
+    return formatShim.createOutputFormat( FormatShim.FormatType.PARQUET, configuration, schemaDescription );
+  }
+
+  @Override public Configuration createConfiguration() {
+    return hadoopConfiguration.getHadoopShim().createConfiguration();
+  }
+
+}
--- kettle-plugins/formats/src/main/java/org/pentaho/big/data/kettle/plugins/formats/parquet/output/ParquetOutput.java
+++ kettle-plugins/formats/src/main/java/org/pentaho/big/data/kettle/plugins/formats/parquet/output/ParquetOutput.java
@@ -22,7 +22,11 @@
 
 package org.pentaho.big.data.kettle.plugins.formats.parquet.output;
 
+import org.pentaho.big.data.api.cluster.service.locator.NamedClusterServiceLocator;
+import org.pentaho.big.data.api.initializer.ClusterInitializationException;
+import org.pentaho.bigdata.api.format.FormatService;
 import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.row.ValueMetaInterface;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.BaseStep;
@@ -30,21 +34,56 @@ import org.pentaho.di.trans.step.StepDataInterface;
 import org.pentaho.di.trans.step.StepInterface;
 import org.pentaho.di.trans.step.StepMeta;
 import org.pentaho.di.trans.step.StepMetaInterface;
+import org.pentaho.hadoop.shim.api.Configuration;
+import org.pentaho.hadoop.shim.api.format.SchemaDescription;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class ParquetOutput extends BaseStep implements StepInterface {
 
+  private final NamedClusterServiceLocator namedClusterServiceLocator;
+  //private final FormatService formatService;
   private ParquetOutputMetaBase meta;
 
   private ParquetOutputData data;
 
   public ParquetOutput( StepMeta stepMeta, StepDataInterface stepDataInterface, int copyNr, TransMeta transMeta,
-      Trans trans ) {
+                        Trans trans, NamedClusterServiceLocator namedClusterServiceLocator ) {
     super( stepMeta, stepDataInterface, copyNr, transMeta, trans );
+    this.namedClusterServiceLocator = namedClusterServiceLocator;
+  }
+
+  public static SchemaDescription makeScheme() {
+    //this part should come from step data
+    SchemaDescription s = new SchemaDescription();
+    s.addField( s.new Field( "b", "Name", ValueMetaInterface.TYPE_STRING ) );
+    s.addField( s.new Field( "c", "Age", ValueMetaInterface.TYPE_STRING ) );
+    return s;
   }
 
   @Override
   public synchronized boolean processRow( StepMetaInterface smi, StepDataInterface sdi ) throws KettleException {
-    throw new KettleException( "Requires Shim API changes" );
+    ParquetOutputMeta parquetOutputMeta = (ParquetOutputMeta) smi;
+    FormatService formatService = null;
+    try {
+      formatService = namedClusterServiceLocator.getService( parquetOutputMeta.getNamedCluster(), FormatService.class );
+
+      Configuration configuration = formatService.createConfiguration();
+      Path tempFile = null;
+      try {
+        tempFile = Files.createTempDirectory( "parquet" );
+      } catch ( IOException e ) {
+        throw new RuntimeException( "error writing to temp file parquet ", e );
+      }
+      //here should be builder to set property
+      configuration.set( "mapreduce.output.fileoutputformat.outputdir", tempFile.toString() );
+      formatService.getOutputFormat( configuration, makeScheme() );
+    } catch ( ClusterInitializationException e ) {
+      throw new RuntimeException( "can't get service forma shim ", e );
+    }
+    return true;
     /*  meta = (ParquetOutputMetaBase) smi;
     data = (ParquetOutputData) sdi;
 
@@ -62,7 +101,7 @@ public class ParquetOutput extends BaseStep implements StepInterface {
       // Path path = shim.getFileSystem( conf ).asPath( file.getName().getURI() );
       
 //      HadoopConfiguration hadoopConfiguration =
-//          HadoopConfigurationBootstrap.getHadoopConfigurationProvider().getActiveConfiguration();
+//          HadoopConfigurationBootstrap.getHadoopConfigurationProvider().createConfiguration();
 //        HadoopShim shim = hadoopConfiguration.getHadoopShim();
 //        org.pentaho.hadoop.shim.api.Configuration conf = shim.createConfiguration();
 //        org.pentaho.hadoop.shim.api.fs.FileSystem fs = shim.getFileSystem( conf );
@@ -118,7 +157,8 @@ public class ParquetOutput extends BaseStep implements StepInterface {
   }
 
   public void writeRow( Object[] row ) throws KettleException {
-    throw new KettleException( "Requires Shim API changes" );
+    //throw new KettleException( "Requires Shim API changes" );
+    System.out.println( "write row" );
    /* GenericRecord record = new GenericData.Record( data.avroSchema );
     List<ParquetOutputField> outputFields = meta.getOutputFields();
     for ( ParquetOutputField field : outputFields ) {
