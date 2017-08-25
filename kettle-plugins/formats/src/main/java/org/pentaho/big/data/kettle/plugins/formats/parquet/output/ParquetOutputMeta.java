a062f92ac5044fcd917ca70a6cbd81a81d03bb10
tgf <tiago.n.ferreira@gmail.com>
2017-08-25 15:49:32 +0100

    [BACKLOG-17671] parquet output dialog save/load

--- kettle-plugins/formats/src/main/java/org/pentaho/big/data/kettle/plugins/formats/parquet/output/ParquetOutputMeta.java
+++ kettle-plugins/formats/src/main/java/org/pentaho/big/data/kettle/plugins/formats/parquet/output/ParquetOutputMeta.java
@@ -22,17 +22,31 @@
 
 package org.pentaho.big.data.kettle.plugins.formats.parquet.output;
 
+import java.util.List;
+import java.util.function.Function;
+
 import org.pentaho.big.data.api.cluster.NamedCluster;
 import org.pentaho.big.data.api.cluster.NamedClusterService;
 import org.pentaho.big.data.api.cluster.service.locator.NamedClusterServiceLocator;
 import org.pentaho.di.core.annotations.Step;
+import org.pentaho.di.core.database.DatabaseMeta;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleXMLException;
+import org.pentaho.di.core.injection.Injection;
+import org.pentaho.di.core.util.StringUtil;
 import org.pentaho.di.core.util.Utils;
+import org.pentaho.di.core.variables.VariableSpace;
+import org.pentaho.di.core.xml.XMLHandler;
 import org.pentaho.di.i18n.BaseMessages;
+import org.pentaho.di.repository.ObjectId;
+import org.pentaho.di.repository.Repository;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.StepDataInterface;
 import org.pentaho.di.trans.step.StepInterface;
 import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.metastore.api.IMetaStore;
+import org.w3c.dom.Node;
 
 @Step( id = "ParquetOutput", image = "PO.svg", name = "ParquetOutput.Name", description = "ParquetOutput.Description",
     categoryDescription = "i18n:org.pentaho.di.trans.step:BaseStep.Category.BigData",
@@ -45,11 +59,19 @@ public class ParquetOutputMeta extends ParquetOutputMetaBase {
   private final NamedClusterServiceLocator namedClusterServiceLocator;
   private final NamedClusterService namedClusterService;
 
-  private EncodingType encodingType;
-  private CompressionType compressionType;
-  private ParquetVersion parquetVersion;
+  @Injection( name = FieldNames.COMPRESSION )
+  private String compressionType;
+  @Injection( name = FieldNames.VERSION )
+  private String parquetVersion;
+  @Injection( name = FieldNames.ROW_GROUP_SIZE )
+  private String rowGroupSize;
+  @Injection( name = FieldNames.DATA_PAGE_SIZE )
+  private String dataPageSize;
+  @Injection( name = FieldNames.ENCODING )
+  private String encodingType;
+  @Injection( name = FieldNames.DICT_PAGE_SIZE )
+  private String dictPageSize;
 
-  // private final NamedClusterLoadSaveUtil namedClusterLoadSaveUtil;
 
   public ParquetOutputMeta( NamedClusterServiceLocator namedClusterServiceLocator,
       NamedClusterService namedClusterService ) {
@@ -72,28 +94,149 @@ public class ParquetOutputMeta extends ParquetOutputMetaBase {
     return new ParquetOutputData();
   }
 
-  public String getEncoding() {
-    return encodingType == null ? EncodingType.PLAIN.toString() : encodingType.toString();
+  public EncodingType getEncodingType( VariableSpace vspace ) {
+    return parseReplace( encodingType, vspace, str -> EncodingType.valueOf( str ), EncodingType.PLAIN );
   }
 
-  public void setEncoding( String encoding ) {
-    encodingType = parseFromToString( encoding, EncodingType.values(), null );
+  public void setEncodingType( String encoding ) {
+    encodingType = StringUtil.isVariable( encoding ) ? encoding
+        : parseFromToString( encoding, EncodingType.values(), EncodingType.PLAIN ).name();
   }
 
-  public String getCompression() {
-    return compressionType == null ? CompressionType.NONE.toString() : compressionType.toString();
+  public String getEncodingType() {
+    return StringUtil.isVariable( encodingType ) ? encodingType : getEncodingType( null ).toString();
   }
 
-  public void setCompression( String compression ) {
-    compressionType = parseFromToString( compression, CompressionType.values(), null );
+  public int getRowGroupSize( VariableSpace vspace ) {
+    return parseReplace( rowGroupSize, vspace, str -> Integer.parseInt( str ), 0 );
+  }
+
+  public String getRowGroupSize() {
+    return rowGroupSize;
+  }
+
+  public void setRowGroupSize( String value ) {
+    rowGroupSize = value;
+  }
+
+  public String getCompressionType() {
+    return StringUtil.isVariable( compressionType ) ? compressionType : getCompressionType( null ).toString();
+  }
+  public void setCompressionType( String value ) {
+    compressionType = StringUtil.isVariable( value ) ? value
+        : parseFromToString( value, CompressionType.values(), null ).name();
+  }
+  public CompressionType getCompressionType( VariableSpace vspace ) {
+    return parseReplace( compressionType, vspace, str -> CompressionType.valueOf( str ), CompressionType.NONE );
   }
 
   public String getParquetVersion() {
-    return parquetVersion == null ? ParquetVersion.PARQUET_1.toString() : parquetVersion.toString();
+    return StringUtil.isVariable( parquetVersion ) ? parquetVersion : getParquetVersion( null ).toString();
   }
 
-  public void setParquetVersion( String version ) {
-    this.parquetVersion = parseFromToString( version, ParquetVersion.values(), null );
+  public void setParquetVersion( String value ) {
+    parquetVersion = StringUtil.isVariable( value ) ? value
+        : parseFromToString( value, ParquetVersion.values(), null ).name();
+  }
+
+  public ParquetVersion getParquetVersion( VariableSpace vspace ) {
+    return parseReplace( parquetVersion, vspace, str -> ParquetVersion.valueOf( str ), ParquetVersion.PARQUET_1 );
+  }
+
+  public int getDataPageSize( VariableSpace vspace ) {
+    return parseReplace( dataPageSize, vspace, s -> Integer.parseInt( s ), 0 );
+  }
+
+  public String getDataPageSize() {
+    return dataPageSize;
+  }
+
+  public void setDataPageSize( String dataPageSize ) {
+    this.dataPageSize = dataPageSize;
+  }
+
+  public int getDictPageSize( VariableSpace vspace ) {
+    return getEncodingType( vspace ).equals( EncodingType.DICTIONARY )
+        ? parseReplace( dictPageSize, vspace, s -> Integer.parseInt( s ), 0 )
+        : 0;
+  }
+
+  public String getDictPageSize() {
+    return dictPageSize;
+  }
+
+  public void setDictPageSize( String dictPageSize ) {
+    this.dictPageSize = dictPageSize;
+  }
+
+  private static class FieldNames {
+    public static final String DICT_PAGE_SIZE = "dictPageSize";
+    public static final String DATA_PAGE_SIZE = "dataPageSize";
+    public static final String ROW_GROUP_SIZE = "rowGroupSize";
+    public static final String VERSION = "version";
+    public static final String ENCODING = "encoding";
+    public static final String COMPRESSION = "compression";
+
+  }
+
+  @Override
+  public void loadXML( Node stepnode, List<DatabaseMeta> databases, IMetaStore metaStore ) throws KettleXMLException {
+    super.loadXML( stepnode, databases, metaStore );
+    encodingType = XMLHandler.getTagValue( stepnode, FieldNames.ENCODING );
+    compressionType = XMLHandler.getTagValue( stepnode, FieldNames.COMPRESSION );
+    parquetVersion = XMLHandler.getTagValue( stepnode, FieldNames.VERSION );
+    rowGroupSize = XMLHandler.getTagValue( stepnode, FieldNames.ROW_GROUP_SIZE );
+    dataPageSize = XMLHandler.getTagValue( stepnode, FieldNames.DATA_PAGE_SIZE );
+    dictPageSize = XMLHandler.getTagValue(  stepnode,  FieldNames.DICT_PAGE_SIZE );
+  }
+
+  private  <T> T parseReplace( String value, VariableSpace vspace, Function<String, T> parser, T defaultValue ) {
+    String replaced = vspace != null ? vspace.environmentSubstitute( value ) : value;
+    if ( !Utils.isEmpty( replaced ) ) {
+      try {
+        return parser.apply( replaced );
+      } catch ( Exception e ) {
+        // ignored
+      }
+    }
+    return defaultValue;
+  }
+
+  @Override
+  public String getXML() {
+    StringBuffer retval = new StringBuffer( super.getXML() );
+    final String INDENT = "    ";
+    retval.append( INDENT ).append( XMLHandler.addTagValue( FieldNames.COMPRESSION, compressionType ) );
+    retval.append( INDENT ).append( XMLHandler.addTagValue( FieldNames.VERSION, parquetVersion ) );
+    retval.append( INDENT ).append( XMLHandler.addTagValue( FieldNames.ENCODING, encodingType ) );
+    retval.append( INDENT ).append( XMLHandler.addTagValue( FieldNames.DICT_PAGE_SIZE, dictPageSize ) );
+    retval.append( INDENT ).append( XMLHandler.addTagValue( FieldNames.ROW_GROUP_SIZE, rowGroupSize ) );
+    retval.append( INDENT ).append( XMLHandler.addTagValue( FieldNames.DATA_PAGE_SIZE, dataPageSize ) );
+    return retval.toString();
+  }
+
+  @Override
+  public void saveRep( Repository rep, IMetaStore metaStore, ObjectId id_transformation, ObjectId id_step )
+    throws KettleException {
+    super.saveRep( rep, metaStore, id_transformation, id_step );
+    rep.saveStepAttribute( id_transformation, id_step, FieldNames.COMPRESSION, compressionType );
+    rep.saveStepAttribute( id_transformation, id_step, FieldNames.VERSION, parquetVersion );
+    rep.saveStepAttribute( id_transformation, id_step, FieldNames.ENCODING, encodingType );
+    rep.saveStepAttribute( id_transformation, id_step, FieldNames.DICT_PAGE_SIZE, dictPageSize );
+    rep.saveStepAttribute( id_transformation, id_step, FieldNames.ROW_GROUP_SIZE, rowGroupSize );
+    rep.saveStepAttribute( id_transformation, id_step, FieldNames.DATA_PAGE_SIZE, dataPageSize );
+  }
+
+  @Override
+  public void readRep( Repository rep, IMetaStore metaStore, ObjectId id_step, List<DatabaseMeta> databases )
+    throws KettleException {
+    super.readRep( rep, metaStore, id_step, databases );
+    compressionType = rep.getStepAttributeString( id_step, FieldNames.COMPRESSION );
+    parquetVersion = rep.getStepAttributeString( id_step, FieldNames.VERSION );
+    encodingType = rep.getStepAttributeString( id_step, FieldNames.ENCODING );
+    dictPageSize = rep.getStepAttributeString( id_step, FieldNames.DICT_PAGE_SIZE );
+    rowGroupSize = rep.getStepAttributeString( id_step, FieldNames.ROW_GROUP_SIZE );
+    dataPageSize = rep.getStepAttributeString( id_step, FieldNames.DATA_PAGE_SIZE );
   }
 
   public String[] getEncodingTypes() {
@@ -109,49 +252,54 @@ public class ParquetOutputMeta extends ParquetOutputMetaBase {
   }
 
   public static enum CompressionType {
-    NONE( getMsg( "ParquetOutput.CompressionType.NONE" ) ), SNAPPY( "Snappy" ), GZIP( "GZIP" ), LZO( "LZO" );
+    NONE( getMsg( "ParquetOutput.CompressionType.NONE" ) ),
+    SNAPPY( "Snappy" ),
+    GZIP( "GZIP" ),
+    LZO( "LZO" );
 
-    private final String name;
+    private final String uiName;
 
     private CompressionType( String name ) {
-      this.name = name;
+      this.uiName = name;
     }
 
     @Override
     public String toString() {
-      return name;
+      return uiName;
     }
   }
 
   public static enum EncodingType {
-    PLAIN( getMsg( "ParquetOutput.EncodingType.PLAIN" ) ), DICTIONARY( getMsg(
-        "ParquetOutput.EncodingType.DICTIONARY" ) ), BIT_PACKED( getMsg(
-            "ParquetOutput.EncodingType.BIT_PACKED" ) ), RLE( getMsg( "ParquetOutput.EncodingType.RLE" ) );
+    PLAIN( getMsg( "ParquetOutput.EncodingType.PLAIN" ) ),
+    DICTIONARY( getMsg( "ParquetOutput.EncodingType.DICTIONARY" ) ),
+    BIT_PACKED( getMsg( "ParquetOutput.EncodingType.BIT_PACKED" ) ),
+    RLE( getMsg( "ParquetOutput.EncodingType.RLE" ) );
 
-    private final String name;
+    private final String uiName;
 
     private EncodingType( String name ) {
-      this.name = name;
+      this.uiName = name;
     }
 
     @Override
     public String toString() {
-      return name;
+      return uiName;
     }
   }
 
   public static enum ParquetVersion {
-    PARQUET_1( "Parquet 1.0" ), PARQUET_2( "Parquet 2.0" );
+    PARQUET_1( "Parquet 1.0" ),
+    PARQUET_2( "Parquet 2.0" );
 
-    private final String name;
+    private final String uiName;
 
     private ParquetVersion( String name ) {
-      this.name = name;
+      this.uiName = name;
     }
 
     @Override
     public String toString() {
-      return name;
+      return uiName;
     }
   }
 
@@ -167,7 +315,7 @@ public class ParquetOutputMeta extends ParquetOutputMetaBase {
   protected static <T> T parseFromToString( String str, T[] values, T defaultValue ) {
     if ( !Utils.isEmpty( str ) ) {
       for ( T type : values ) {
-        if ( str.equalsIgnoreCase( type.toString() ) ) {
+        if ( str.equals( type.toString() ) ) {
           return type;
         }
       }
