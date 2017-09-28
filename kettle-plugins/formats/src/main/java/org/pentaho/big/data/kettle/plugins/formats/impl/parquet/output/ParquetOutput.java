7d03165b30b7c2bbccf8f562e83f32dffc411aa1
Alexander Buloichik <alexander_buloichik@epam.com>
2017-09-28 15:50:13 +0300

    [BACKLOG-18970] Add Date/Time to filename

--- kettle-plugins/formats/src/main/java/org/pentaho/big/data/kettle/plugins/formats/impl/parquet/output/ParquetOutput.java
+++ kettle-plugins/formats/src/main/java/org/pentaho/big/data/kettle/plugins/formats/impl/parquet/output/ParquetOutput.java
@@ -23,6 +23,7 @@
 package org.pentaho.big.data.kettle.plugins.formats.impl.parquet.output;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 import org.apache.commons.vfs2.FileObject;
 import org.pentaho.big.data.api.cluster.service.locator.NamedClusterServiceLocator;
@@ -103,6 +104,17 @@ public class ParquetOutput extends BaseStep implements StepInterface {
     data.output = formatService.createOutputFormat( IPentahoParquetOutputFormat.class );
 
     String outputFileName = meta.getFilename();
+    if ( meta.dateTimeFormat != null ) {
+      outputFileName += new SimpleDateFormat( meta.dateTimeFormat ).format( new Date() );
+    } else {
+      if ( meta.dateInFilename ) {
+        outputFileName += '_' + new SimpleDateFormat( "yyyyMMdd" ).format( new Date() );
+      }
+      if ( meta.timeInFilename ) {
+        outputFileName += '_' + new SimpleDateFormat( "HHmmss" ).format( new Date() );
+      }
+    }
+    outputFileName += meta.extension;
     FileObject outputFileObject = KettleVFS.getFileObject( outputFileName );
     if ( AliasedFileObject.isAliasedFile( outputFileObject ) ) {
       outputFileName = ( (AliasedFileObject) outputFileObject ).getOriginalURIString();
@@ -118,8 +130,9 @@ public class ParquetOutput extends BaseStep implements StepInterface {
       compression = IPentahoParquetOutputFormat.COMPRESSION.UNCOMPRESSED;
     }
     data.output.setCompression( compression );
-    data.output.setVersion( "Parquet 1.0".equals( meta.getParquetVersion() )
-        ? IPentahoParquetOutputFormat.VERSION.VERSION_1_0 : IPentahoParquetOutputFormat.VERSION.VERSION_2_0 );
+    data.output
+        .setVersion( IPentahoParquetOutputFormat.VERSION.VERSION_1_0.toString().equals( meta.getParquetVersion() )
+            ? IPentahoParquetOutputFormat.VERSION.VERSION_1_0 : IPentahoParquetOutputFormat.VERSION.VERSION_2_0 );
     if ( meta.getRowGroupSize( variables ) > 0 ) {
       data.output.setRowGroupSize( meta.getRowGroupSize( variables ) * 1024 * 1024 );
     }
