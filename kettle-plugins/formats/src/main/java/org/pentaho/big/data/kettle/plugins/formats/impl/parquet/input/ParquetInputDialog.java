275101fdd678e0beca0425ad29920ff133f004d1
Alexander Buloichik <alexander_buloichik@epam.com>
2017-11-30 13:27:33 +0300

    [BACKLOG-20045] Add dialog for Get fields and mark meta as changed after Get Fields

--- kettle-plugins/formats/src/main/java/org/pentaho/big/data/kettle/plugins/formats/impl/parquet/input/ParquetInputDialog.java
+++ kettle-plugins/formats/src/main/java/org/pentaho/big/data/kettle/plugins/formats/impl/parquet/input/ParquetInputDialog.java
@@ -25,7 +25,10 @@ package org.pentaho.big.data.kettle.plugins.formats.impl.parquet.input;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -40,6 +43,7 @@ import org.pentaho.big.data.kettle.plugins.formats.impl.parquet.BaseParquetStepD
 import org.pentaho.big.data.kettle.plugins.formats.parquet.input.ParquetInputMetaBase;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.row.value.ValueMetaFactory;
+import org.pentaho.di.core.util.Utils;
 import org.pentaho.di.i18n.BaseMessages;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
@@ -47,6 +51,7 @@ import org.pentaho.di.trans.TransPreviewFactory;
 import org.pentaho.di.ui.core.dialog.EnterNumberDialog;
 import org.pentaho.di.ui.core.dialog.EnterTextDialog;
 import org.pentaho.di.ui.core.dialog.PreviewRowsDialog;
+import org.pentaho.di.ui.core.gui.GUIResource;
 import org.pentaho.di.ui.core.widget.ColumnInfo;
 import org.pentaho.di.ui.core.widget.ColumnsResizer;
 import org.pentaho.di.ui.core.widget.TableView;
@@ -54,6 +59,8 @@ import org.pentaho.di.ui.trans.dialog.TransPreviewProgressDialog;
 import org.pentaho.hadoop.shim.api.format.SchemaDescription;
 
 public class ParquetInputDialog extends BaseParquetStepDialog<ParquetInputMeta> {
+
+  private static final Class<?> PKG = ParquetInputMeta.class;
 
   private static final int DIALOG_WIDTH = 526;
 
@@ -78,8 +85,7 @@ public class ParquetInputDialog extends BaseParquetStepDialog<ParquetInputMeta> 
     wGetFields.setText( BaseMessages.getString( PKG, "ParquetInputDialog.Fields.Get" ) );
     wGetFields.addListener( SWT.Selection, event -> {
       try {
-        setFields( ParquetInput.retrieveSchema( meta.namedClusterServiceLocator, meta.getNamedCluster(), wPath.getText()
-          .trim() ) );
+        getFields( );
       } catch ( ClusterInitializationException ex ) {
         if ( !BaseParquetStepDialog.checkForNonActiveShim( ex ) ) {
           throw new RuntimeException( ex );
@@ -151,15 +157,48 @@ public class ParquetInputDialog extends BaseParquetStepDialog<ParquetInputMeta> 
     }
   }
 
-  protected void setFields( SchemaDescription schema ) {
-    wInputFields.table.removeAll();
+  protected void getFields() throws Exception {
+    SchemaDescription schema =
+        ParquetInput.retrieveSchema( meta.namedClusterServiceLocator, meta.getNamedCluster(), wPath.getText().trim() );
+
+    if ( schema.isEmpty() ) {
+      return;
+    }
+    Set<String> existKeys = new TreeSet<>();
+    if ( wInputFields.table.getItemCount() > 0 ) {
+      MessageDialog dialog = getFieldsChoiceDialog( shell, schema.getFieldsCount() );
+      int idx = dialog.open();
+      int choice = idx & 0xFF;
+      switch ( choice ) {
+        case 3:
+        case 255:
+          return; // cancel
+        case 2:
+          wInputFields.table.removeAll();
+          break;
+        case 0:
+          for ( int i = 0; i < wInputFields.table.getItemCount(); i++ ) {
+            TableItem tableItem = wInputFields.table.getItem( i );
+            String key = tableItem.getText( FIELD_NAME_COLUMN_INDEX );
+            if ( !Utils.isEmpty( key ) ) {
+              existKeys.add( key );
+            }
+          }
+          break;
+      }
+    }
     for ( SchemaDescription.Field f : schema ) {
+      if ( existKeys.contains( f.formatFieldName ) ) {
+        continue;
+      }
       TableItem item = new TableItem( wInputFields.table, SWT.NONE );
 
       item.setText( AVRO_PATH_COLUMN_INDEX, f.formatFieldName );
       item.setText( FIELD_NAME_COLUMN_INDEX, f.formatFieldName );
       item.setText( FIELD_TYPE_COLUMN_INDEX, ValueMetaFactory.getValueMetaName( f.pentahoValueMetaType ) );
     }
+
+    meta.setChanged();
   }
 
   /**
@@ -261,5 +300,45 @@ public class ParquetInputDialog extends BaseParquetStepDialog<ParquetInputMeta> 
         prd.open();
       }
     };
+  }
+
+  static MessageDialog getFieldsChoiceDialog( Shell shell, int newFields ) {
+    MessageDialog messageDialog =
+      new MessageDialog( shell,
+        BaseMessages.getString( PKG, "ParquetInput.GetFieldsChoice.Title" ), // "Warning!"
+        null,
+        BaseMessages.getString( PKG, "ParquetInput.GetFieldsChoice.Message", "" + newFields ),
+        MessageDialog.WARNING, new String[] {
+        BaseMessages.getString( PKG, "ParquetInput.GetFieldsChoice.AddNew" ),
+        BaseMessages.getString( PKG, "ParquetInput.GetFieldsChoice.Add" ),
+        BaseMessages.getString( PKG, "ParquetInput.GetFieldsChoice.ClearAndAdd" ),
+        BaseMessages.getString( PKG, "ParquetInput.GetFieldsChoice.Cancel" ), }, 0 ) {
+
+        public void create() {
+          super.create();
+          getShell().setBackground( GUIResource.getInstance().getColorWhite() );
+        }
+
+        protected Control createMessageArea( Composite composite ) {
+          Control control = super.createMessageArea( composite );
+          imageLabel.setBackground( GUIResource.getInstance().getColorWhite() );
+          messageLabel.setBackground( GUIResource.getInstance().getColorWhite() );
+          return control;
+        }
+
+        protected Control createDialogArea( Composite parent ) {
+          Control control = super.createDialogArea( parent );
+          control.setBackground( GUIResource.getInstance().getColorWhite() );
+          return control;
+        }
+
+        protected Control createButtonBar( Composite parent ) {
+          Control control = super.createButtonBar( parent );
+          control.setBackground( GUIResource.getInstance().getColorWhite() );
+          return control;
+        }
+      };
+    MessageDialog.setDefaultImage( GUIResource.getInstance().getImageSpoon() );
+    return messageDialog;
   }
 }
