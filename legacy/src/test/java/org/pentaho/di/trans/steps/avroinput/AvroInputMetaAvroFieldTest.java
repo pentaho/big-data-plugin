1586a45ab73358fc73a9dc8910bc615cd0699c29
Luis Martins <luis.martins@hitachivantara.com>
2018-03-04 18:19:45 +0000

    [SP-4206] Backport of PDI-17035 - AVRO input step from 7.1 not compatible with 8.0 (8.0 Suite)

---
+++ legacy/src/test/java/org/pentaho/di/trans/steps/avroinput/AvroInputMetaAvroFieldTest.java
@@ -0,0 +1,410 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.di.trans.steps.avroinput;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.util.Utf8;
+import org.codehaus.jackson.node.IntNode;
+import org.codehaus.jackson.node.LongNode;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettlePluginException;
+import org.pentaho.di.core.plugins.PluginRegistry;
+import org.pentaho.di.core.row.value.ValueMetaBigNumber;
+import org.pentaho.di.core.row.value.ValueMetaBinary;
+import org.pentaho.di.core.row.value.ValueMetaBoolean;
+import org.pentaho.di.core.row.value.ValueMetaDate;
+import org.pentaho.di.core.row.value.ValueMetaInteger;
+import org.pentaho.di.core.row.value.ValueMetaNumber;
+import org.pentaho.di.core.row.value.ValueMetaPluginType;
+import org.pentaho.di.core.row.value.ValueMetaString;
+import org.pentaho.di.core.variables.VariableSpace;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by bryan on 10/21/15.
+ */
+public class AvroInputMetaAvroFieldTest {
+  private AvroInputMeta.AvroField avroField;
+  private VariableSpace variableSpace;
+  private Map<String, String> variableSpaceMap;
+
+  @BeforeClass
+  public static void before() throws KettlePluginException {
+    PluginRegistry.addPluginType( ValueMetaPluginType.getInstance() );
+    PluginRegistry.init( false );
+  }
+
+  @Before
+  public void setup() throws KettleException {
+    avroField = new AvroInputMeta.AvroField();
+    variableSpace = mock( VariableSpace.class );
+    variableSpaceMap = new HashMap<String, String>();
+    when( variableSpace.environmentSubstitute( anyString() ) ).thenAnswer( new Answer<String>() {
+      @Override public String answer( InvocationOnMock invocation ) throws Throwable {
+        Object key = invocation.getArguments()[ 0 ];
+        String result = variableSpaceMap.get( key );
+        if ( result == null ) {
+          return String.valueOf( key );
+        }
+        return result;
+      }
+    } );
+    avroField.m_fieldPath = "testFieldPath";
+  }
+
+  @Test( expected = KettleException.class )
+  public void testInitNoPathSet() throws KettleException {
+    avroField = new AvroInputMeta.AvroField();
+    avroField.init( 0 );
+  }
+
+  @Test
+  public void testGetKettleValueBigNumber() throws KettleException {
+    avroField.m_kettleType = "BigNumber";
+    avroField.init( 0 );
+    BigDecimal bigDecimal = new BigDecimal( 10 );
+    BigDecimal expected = new ValueMetaBigNumber().getBigNumber( bigDecimal );
+    assertEquals( expected, avroField.getKettleValue( bigDecimal ) );
+  }
+
+  @Test
+  public void testGetKettleValueBinary() throws KettleException {
+    avroField.m_kettleType = "Binary";
+    avroField.init( 0 );
+    byte[] bytes = new byte[] { 0, 2, 3 };
+    assertEquals( new ValueMetaBinary().getBinary( bytes ), avroField.getKettleValue( bytes ) );
+  }
+
+  @Test
+  public void testGetKettleValueBoolean() throws KettleException {
+    avroField.m_kettleType = "Boolean";
+    avroField.init( 0 );
+    boolean value = false;
+    Boolean expected = new ValueMetaBoolean().getBoolean( value );
+    assertEquals( expected, avroField.getKettleValue( value ) );
+
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.BOOLEAN );
+    assertEquals( expected, avroField.getPrimitive( value, schema ) );
+  }
+
+  @Test
+  public void testGetKettleValueDate() throws KettleException {
+    avroField.m_kettleType = "Date";
+    avroField.init( 0 );
+    Date date = new Date();
+    assertEquals( new ValueMetaDate().getDate( date ), avroField.getKettleValue( date ) );
+  }
+
+  @Test
+  public void testGetKettleValueInteger() throws KettleException {
+    avroField.m_kettleType = "Integer";
+    avroField.init( 0 );
+    long num = 12;
+    Long expected = new ValueMetaInteger().getInteger( num );
+    assertEquals( expected, avroField.getKettleValue( num ) );
+
+
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.INT );
+    assertEquals( expected, avroField.getPrimitive( Long.valueOf( num ).intValue(), schema ) );
+  }
+
+  @Test
+  public void testGetKettleValueNumber() throws KettleException {
+    avroField.m_kettleType = "Number";
+    avroField.init( 0 );
+    Double number = 2.5;
+    Double expected = new ValueMetaNumber().getNumber( number );
+    assertEquals( expected, avroField.getKettleValue( number ) );
+
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.FLOAT );
+    assertEquals( expected, avroField.getPrimitive( number.floatValue(), schema ) );
+  }
+
+  @Test
+  public void testGetKettleValueString() throws KettleException {
+    avroField.m_kettleType = "String";
+    avroField.init( 0 );
+    String string = "value: 2.54";
+    assertEquals( new ValueMetaString().getString( string ), avroField.getKettleValue( string ) );
+  }
+
+  @Test
+  public void testGetKettleValueInternetAddress() throws KettleException, UnknownHostException {
+    avroField.m_kettleType = "Internet Address";
+    avroField.init( 0 );
+    InetAddress inetAddress = InetAddress.getByName( "192.168.1.1" );
+    assertNull( avroField.getKettleValue( inetAddress ) );
+
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.RECORD );
+    assertNull( avroField.getPrimitive( inetAddress, schema ) );
+  }
+
+  @Test
+  public void testGetPrimitiveNull() throws KettleException {
+    avroField.init( 0 );
+    assertNull( avroField.getPrimitive( null, null ) );
+  }
+
+  @Test
+  public void testGetPrimitiveFixed() throws KettleException, UnsupportedEncodingException {
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.FIXED );
+    GenericData.Fixed fixed = new GenericData.Fixed( schema );
+    String testBytes = "testBytes";
+    fixed.bytes( testBytes.getBytes( "UTF-8" ) );
+    assertEquals( testBytes, new String( (byte[]) avroField.getPrimitive( fixed, schema ), "UTF-8" ) );
+  }
+
+  @Test
+  public void testConvertToKettleValueMapNull() throws KettleException {
+    assertNull( avroField.convertToKettleValue( (Map<Utf8, Object>) null, null, null, false ) );
+  }
+
+  @Test( expected = KettleException.class )
+  public void testConvertToKettleValueMapExceptionWithNoTempParts() throws KettleException {
+    avroField.init( 0 );
+    avroField.convertToKettleValue( Collections.<Utf8, Object>emptyMap(), mock( Schema.class ), mock( Schema.class ), false );
+  }
+
+  @Test( expected = KettleException.class )
+  public void testConvertToKettleValueMapExceptionWithMalformedPath() throws KettleException {
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    avroField.convertToKettleValue( Collections.<Utf8, Object>emptyMap(), mock( Schema.class ), mock( Schema.class ), false );
+  }
+
+  @Test
+  public void testConvertToKettleValueMapNullValue() throws KettleException {
+    avroField.m_fieldPath = "[key]";
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    assertNull( avroField.convertToKettleValue( new HashMap<Utf8, Object>(), mock( Schema.class ), mock( Schema.class ), false ) );
+  }
+
+  @Test
+  public void testConvertToKettleValueMapStringValue() throws KettleException {
+    avroField.m_kettleType = "String";
+    avroField.m_fieldPath = "[key]";
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    Map<Utf8, Object> map = new HashMap<Utf8, Object>();
+    String testString = "testString";
+    map.put( new Utf8( "key" ), testString );
+    Schema schema = mock( Schema.class );
+    Schema valueSchema = mock( Schema.class );
+    when( schema.getValueType() ).thenReturn( valueSchema );
+    when( valueSchema.getType() ).thenReturn( Schema.Type.STRING );
+    assertEquals( testString, avroField.convertToKettleValue( map, schema, mock( Schema.class ), false ) );
+  }
+
+  @Test
+  public void testConvertToKettleValueArrayNull() throws KettleException {
+    assertNull( avroField.convertToKettleValue( (GenericData.Array) null, null, mock( Schema.class ), false ) );
+  }
+
+  @Test( expected = KettleException.class )
+  public void testConvertToKettleValueArrayExceptionWithNoTempParts() throws KettleException {
+    avroField.init( 0 );
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.ARRAY );
+    avroField.convertToKettleValue( new GenericData.Array( 0, schema ), mock( Schema.class ), mock( Schema.class ), false );
+  }
+
+  @Test( expected = KettleException.class )
+  public void testConvertToKettleValueArrayWithMalformedPath() throws KettleException {
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.ARRAY );
+    avroField.convertToKettleValue( new GenericData.Array( 0, schema ), mock( Schema.class ), mock( Schema.class ), false );
+  }
+
+  @Test( expected = KettleException.class )
+  public void testConvertToKettleValueArrayNumberFormatException() throws KettleException {
+    avroField.m_fieldPath = "[key]";
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.ARRAY );
+    avroField.convertToKettleValue( new GenericData.Array( 0, schema ), mock( Schema.class ), mock( Schema.class ), false );
+  }
+
+  @Test
+  public void testConvertToKettleValueArrayIndexLessThanZero() throws KettleException {
+    avroField.m_fieldPath = "[-1]";
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.ARRAY );
+    assertNull( avroField.convertToKettleValue( new GenericData.Array( 0, schema ), mock( Schema.class ), mock( Schema.class ), false ) );
+  }
+
+  @Test
+  public void testConvertToKettleValueArrayIndexTooLarge() throws KettleException {
+    avroField.m_fieldPath = "[1]";
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.ARRAY );
+    assertNull( avroField.convertToKettleValue( new GenericData.Array( 1, schema ), mock( Schema.class ), mock( Schema.class ), false ) );
+  }
+
+  @Test
+  public void testConvertToKettleValueArrayNullElement() throws KettleException {
+    avroField.m_fieldPath = "[0]";
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.ARRAY );
+    GenericData.Array array = new GenericData.Array( 1, schema );
+    array.add( null );
+    assertNull( avroField.convertToKettleValue( array, mock( Schema.class ), mock( Schema.class ), false ) );
+  }
+
+  @Test
+  public void testConvertToKettleValueArrayStringValue() throws KettleException {
+    avroField.m_kettleType = "String";
+    avroField.m_fieldPath = "[0]";
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    String testString = "testString";
+    Schema schema = mock( Schema.class );
+    Schema valueSchema = mock( Schema.class );
+    when( schema.getElementType() ).thenReturn( valueSchema );
+    when( schema.getType() ).thenReturn( Schema.Type.ARRAY );
+    when( valueSchema.getType() ).thenReturn( Schema.Type.STRING );
+    GenericData.Array array = new GenericData.Array( 1, schema );
+    array.add( testString );
+    assertEquals( testString, avroField.convertToKettleValue( array, schema, mock( Schema.class ), false ) );
+  }
+
+  @Test
+  public void testConvertToKettleValueRecordNull() throws KettleException {
+    assertNull( avroField.convertToKettleValue( (GenericData.Record) null, null, mock( Schema.class ), false ) );
+  }
+
+  @Test( expected = KettleException.class )
+  public void testConvertToKettleValueRecordExceptionWithNoTempParts() throws KettleException {
+    avroField.init( 0 );
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.RECORD );
+    avroField.convertToKettleValue( new GenericData.Record( schema ), mock( Schema.class ), mock( Schema.class ), false );
+  }
+
+  @Test( expected = KettleException.class )
+  public void testConvertToKettleValueRecordWithMalformedPath() throws KettleException {
+    avroField.m_fieldPath = "[0]";
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.RECORD );
+    avroField.convertToKettleValue( new GenericData.Record( schema ), mock( Schema.class ), mock( Schema.class ), false );
+  }
+
+  @Test( expected = KettleException.class )
+  public void testConvertToKettleValueRecordNullSchema() throws KettleException {
+    avroField.m_fieldPath = "key";
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    Schema schema = mock( Schema.class );
+    when( schema.getType() ).thenReturn( Schema.Type.RECORD );
+    assertNull( avroField.convertToKettleValue( new GenericData.Record( schema ), mock( Schema.class ), mock( Schema.class ), true ) );
+    avroField.convertToKettleValue( new GenericData.Record( schema ), mock( Schema.class ), mock( Schema.class ), false );
+  }
+
+  @Test
+  public void testConvertToKettleValueRecordStringValue() throws KettleException {
+    avroField.m_kettleType = "String";
+    avroField.m_fieldPath = "key";
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    String testString = "testString";
+    Schema schema = mock( Schema.class );
+    Schema.Field field = mock( Schema.Field.class );
+    when( schema.getFields() ).thenReturn( new ArrayList<>( Arrays.asList( field ) ) );
+    when( schema.getField( avroField.m_fieldPath ) ).thenReturn( field );
+    when( schema.getType() ).thenReturn( Schema.Type.RECORD );
+    Schema fieldSchema = mock( Schema.class );
+    when( field.schema() ).thenReturn( fieldSchema );
+    when( fieldSchema.getType() ).thenReturn( Schema.Type.STRING );
+    GenericData.Record record = new GenericData.Record( schema );
+    record.put( avroField.m_fieldPath, testString );
+    assertEquals( testString, avroField.convertToKettleValue( record, schema, mock( Schema.class ), false ) );
+  }
+
+  @Test
+  public void testGetDefaultValueFromDefaultSchemaIfNull() throws KettleException {
+    avroField.m_kettleType = "Integer";
+    avroField.m_fieldPath = "key";
+    avroField.init( 0 );
+    avroField.reset( variableSpace );
+    IntNode node = new IntNode( 5 );
+    Schema schemaToUse = mock( Schema.class );
+    Schema defaultSchema = mock( Schema.class );
+    Schema fieldSchema = mock( Schema.class );
+    Schema.Field field = mock( Schema.Field.class );
+    GenericData.Record record = mock( GenericData.Record.class );
+    when( record.get( avroField.m_fieldPath ) ).thenReturn( null );
+    when( defaultSchema.getField( avroField.m_fieldPath ) ).thenReturn( field );
+    when( field.defaultValue() ).thenReturn( node ).thenReturn( node );
+    when( field.schema() ).thenReturn( fieldSchema );
+    when( fieldSchema.getType() ).thenReturn( Schema.Type.INT );
+    assertEquals( 5L, avroField.convertToKettleValue( record, schemaToUse, defaultSchema, true ) );
+  }
+
+  @Test
+  public void testGetPrimitiveFromConvertNode() throws KettleException {
+    avroField.m_kettleType = "Integer";
+    avroField.init( 0 );
+    Schema schema = mock( Schema.class );
+    LongNode node = new LongNode( 22 );
+    when( schema.getType() ).thenReturn( Schema.Type.LONG );
+    assertEquals( 22L,  avroField.getPrimitive( node, schema ) );
+  }
+}
---
+++ legacy/src/test/java/org/pentaho/di/trans/steps/avroinput/AvroInputMetaTest.java
@@ -0,0 +1,215 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.di.trans.steps.avroinput;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettlePluginException;
+import org.pentaho.di.core.exception.KettleStepException;
+import org.pentaho.di.core.logging.LogChannelInterface;
+import org.pentaho.di.core.logging.LoggingObjectInterface;
+import org.pentaho.di.core.plugins.PluginRegistry;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.row.ValueMetaInterface;
+import org.pentaho.di.core.row.value.ValueMetaPluginType;
+import org.pentaho.di.core.variables.VariableSpace;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.trans.steps.loadsave.LoadSaveTester;
+import org.pentaho.di.trans.steps.loadsave.validator.FieldLoadSaveValidator;
+import org.pentaho.di.trans.steps.loadsave.validator.ListLoadSaveValidator;
+import org.pentaho.di.trans.steps.loadsave.validator.StringLoadSaveValidator;
+import org.pentaho.di.trans.steps.mock.StepMockHelper;
+
+/**
+ * Created by bryan on 10/21/15.
+ */
+public class AvroInputMetaTest {
+  private AvroInputMeta avroInputMeta;
+
+  @BeforeClass
+  public static void before() throws KettlePluginException {
+    PluginRegistry.addPluginType( ValueMetaPluginType.getInstance() );
+    PluginRegistry.init( false );
+  }
+
+  @Before
+  public void setup() {
+    avroInputMeta = new AvroInputMeta();
+  }
+
+  @Test
+  public void testLoadSave() throws KettleException {
+    List<String> commonAttributes = new ArrayList<>();
+    commonAttributes.add( "avroInField" );
+    commonAttributes.add( "avroFieldName" );
+    commonAttributes.add( "schemaInField" );
+    commonAttributes.add( "schemaFieldName" );
+    commonAttributes.add( "schemaInFieldIsPath" );
+    commonAttributes.add( "cacheSchemasInMemory" );
+    commonAttributes.add( "filename" );
+    commonAttributes.add( "schemaFilename" );
+    commonAttributes.add( "avroIsJsonEncoded" );
+    commonAttributes.add( "avroFields" );
+    commonAttributes.add( "lookupFields" );
+    commonAttributes.add( "dontComplainAboutMissingFields" );
+
+    Map<String, FieldLoadSaveValidator<?>> fieldLoadSaveValidatorTypeMap = new HashMap<>();
+
+    Map<String, FieldLoadSaveValidator<?>> fieldLoadSaveValidatorAttributeMap = new HashMap<>();
+    fieldLoadSaveValidatorAttributeMap.put( "avroFields", new ListLoadSaveValidator<AvroInputMeta.AvroField>(
+      new FieldLoadSaveValidator<AvroInputMeta.AvroField>() {
+        private final StringLoadSaveValidator stringLoadSaveValidator = new StringLoadSaveValidator();
+        private final ListLoadSaveValidator<String> stringListLoadSaveValidator =
+          new ListLoadSaveValidator<>( stringLoadSaveValidator );
+
+        @Override public AvroInputMeta.AvroField getTestObject() {
+          AvroInputMeta.AvroField avroField = new AvroInputMeta.AvroField();
+          avroField.m_kettleType = stringLoadSaveValidator.getTestObject();
+          avroField.m_fieldPath = stringLoadSaveValidator.getTestObject();
+          avroField.m_fieldName = stringLoadSaveValidator.getTestObject();
+          avroField.m_indexedVals = stringListLoadSaveValidator.getTestObject();
+          return avroField;
+        }
+
+        @Override public boolean validateTestObject( AvroInputMeta.AvroField avroField, Object o ) {
+          if ( !( o instanceof AvroInputMeta.AvroField ) ) {
+            return false;
+          }
+          AvroInputMeta.AvroField avroField2 = (AvroInputMeta.AvroField) o;
+          return stringLoadSaveValidator.validateTestObject( avroField.m_kettleType, avroField2.m_kettleType )
+            && stringLoadSaveValidator.validateTestObject( avroField.m_fieldPath, avroField2.m_fieldPath )
+            && stringLoadSaveValidator.validateTestObject( avroField.m_fieldName, avroField2.m_fieldName )
+            && stringListLoadSaveValidator.validateTestObject( avroField.m_indexedVals, avroField2.m_indexedVals );
+        }
+      } ) );
+    fieldLoadSaveValidatorAttributeMap.put( "lookupFields", new ListLoadSaveValidator<AvroInputMeta.LookupField>(
+      new FieldLoadSaveValidator<AvroInputMeta.LookupField>() {
+        private final StringLoadSaveValidator stringLoadSaveValidator = new StringLoadSaveValidator();
+
+        @Override public AvroInputMeta.LookupField getTestObject() {
+          AvroInputMeta.LookupField lookupField = new AvroInputMeta.LookupField();
+          lookupField.m_fieldName = stringLoadSaveValidator.getTestObject();
+          lookupField.m_variableName = stringLoadSaveValidator.getTestObject();
+          lookupField.m_defaultValue = stringLoadSaveValidator.getTestObject();
+          return lookupField;
+        }
+
+        @Override public boolean validateTestObject( AvroInputMeta.LookupField lookupField, Object o ) {
+          if ( !( o instanceof AvroInputMeta.LookupField ) ) {
+            return false;
+          }
+          AvroInputMeta.LookupField lookupField2 = (AvroInputMeta.LookupField) o;
+          return stringLoadSaveValidator.validateTestObject( lookupField.m_fieldName, lookupField2.m_fieldName )
+            && stringLoadSaveValidator.validateTestObject( lookupField.m_variableName, lookupField2.m_variableName )
+            && stringLoadSaveValidator.validateTestObject( lookupField.m_defaultValue, lookupField2.m_defaultValue );
+        }
+      } ) );
+
+    LoadSaveTester<AvroInputMeta> avroInputMetaLoadSaveTester =
+      new LoadSaveTester<AvroInputMeta>( AvroInputMeta.class, commonAttributes, new HashMap<String, String>(),
+        new HashMap<String, String>(), fieldLoadSaveValidatorAttributeMap, fieldLoadSaveValidatorTypeMap );
+
+    avroInputMetaLoadSaveTester.testSerialization();
+  }
+
+  @Test
+  public void testGetStepData() {
+    assertTrue( avroInputMeta.getStepData() instanceof AvroInputData );
+  }
+
+  @Test
+  public void testGetStep() {
+    StepMockHelper<AvroInputMeta, AvroInputData> stepMockHelper =
+      new StepMockHelper<AvroInputMeta, AvroInputData>( "avro", AvroInputMeta.class, AvroInputData.class );
+    when( stepMockHelper.logChannelInterfaceFactory.create( anyObject(), any( LoggingObjectInterface.class ) ) )
+      .thenReturn( mock( LogChannelInterface.class ) );
+    assertTrue( avroInputMeta
+      .getStep( stepMockHelper.stepMeta, stepMockHelper.stepDataInterface, 0, stepMockHelper.transMeta,
+        stepMockHelper.trans ) instanceof AvroInput );
+  }
+
+  @Test
+  public void testIndexedValsList() {
+    String val1 = "val1";
+    String val2 = "val2";
+    String val3 = "val3";
+    List<String> indexedValsList = AvroInputMeta.indexedValsList( val1 + " , " + val2 + "," + val3 + " " );
+    assertEquals( new ArrayList<>( Arrays.asList( val1, val2, val3 ) ), indexedValsList );
+    assertEquals( val1 + "," + val2 + "," + val3, AvroInputMeta.indexedValsList( indexedValsList ) );
+  }
+
+  @Test
+  public void testSetDefault() {
+    // Doesn't do anything
+    avroInputMeta.setDefault();
+  }
+
+  @Test
+  public void testGetDialogClassName() {
+    assertEquals( AvroInputDialog.class.getCanonicalName(), avroInputMeta.getDialogClassName() );
+  }
+
+  @Test
+  public void testSupportsErrorHandling() {
+    assertTrue( avroInputMeta.supportsErrorHandling() );
+  }
+
+  @Test
+  public void testGetFieldsMFields() throws KettleStepException {
+    AvroInputMeta.AvroField avroField = new AvroInputMeta.AvroField();
+    avroField.m_fieldName = "testFieldName";
+    avroField.m_kettleType = "String";
+    String testOrigin = "testOrigin";
+    String abc = "abc";
+    avroField.m_indexedVals = new ArrayList<>( Arrays.asList( abc ) );
+    avroInputMeta.setAvroFields( new ArrayList<>( Arrays.asList( avroField ) ) );
+    RowMetaInterface rowMeta = mock( RowMetaInterface.class );
+    avroInputMeta.getFields( rowMeta, testOrigin, new RowMetaInterface[ 0 ], mock( StepMeta.class ),
+      mock( VariableSpace.class ) );
+    ArgumentCaptor<ValueMetaInterface> valueMetaInterfaceArgumentCaptor = ArgumentCaptor.forClass( ValueMetaInterface.class );
+    verify( rowMeta ).addValueMeta( valueMetaInterfaceArgumentCaptor.capture() );
+    ValueMetaInterface valueMetaInterface = valueMetaInterfaceArgumentCaptor.getValue();
+    assertEquals( avroField.m_fieldName, valueMetaInterface.getName() );
+    assertEquals( testOrigin, valueMetaInterface.getOrigin() );
+    assertEquals( ValueMetaInterface.TYPE_STRING, valueMetaInterface.getType() );
+    assertArrayEquals( new Object[] { abc }, valueMetaInterface.getIndex() );
+  }
+}
