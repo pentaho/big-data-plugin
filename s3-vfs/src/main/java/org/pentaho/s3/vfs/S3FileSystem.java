7d66b9407c5adeca4fed43b89b42e9c5fb7f8aec
rmansoor <rmansoor@pentaho.com>
2018-04-30 16:45:39 -0400

    [CLEANUP]  [BACKLOG-23110] - Inject access key and secret key to system property when using s3 protocol. Values are cleared when the step is finished

--- s3-vfs/src/main/java/org/pentaho/s3/vfs/S3FileSystem.java
+++ s3-vfs/src/main/java/org/pentaho/s3/vfs/S3FileSystem.java
@@ -37,6 +37,16 @@ import org.jets3t.service.security.ProviderCredentials;
 public class S3FileSystem extends AbstractFileSystem implements FileSystem {
 
   private S3Service service;
+  /** System property name for the AWS access key ID */
+  public static final String ACCESS_KEY_SYSTEM_PROPERTY = "aws.accessKeyId";
+
+  /** System property name for the AWS secret key */
+  public  static final String SECRET_KEY_SYSTEM_PROPERTY = "aws.secretKey";
+
+  private String awsAccessKeyCache;
+
+  private String awsSecretKeyCache;
+
 
   protected S3FileSystem( final FileName rootName, final FileSystemOptions fileSystemOptions ) {
     super( rootName, null, fileSystemOptions );
@@ -52,8 +62,13 @@ public class S3FileSystem extends AbstractFileSystem implements FileSystem {
   }
 
   public S3Service getS3Service() {
+    String awsAccessKeySystemEnvValue = System.getProperty( ACCESS_KEY_SYSTEM_PROPERTY );
+    String awsSecretKeySystemEnvValue = System.getProperty( SECRET_KEY_SYSTEM_PROPERTY );
+
     if ( service == null || service.getProviderCredentials() == null
-      || service.getProviderCredentials().getAccessKey() == null ) {
+      || service.getProviderCredentials().getAccessKey() == null || ( service != null
+        && hasChanged( awsAccessKeyCache, awsAccessKeySystemEnvValue )
+            && hasChanged( awsSecretKeyCache, awsSecretKeySystemEnvValue ) ) ) {
 
       UserAuthenticator userAuthenticator =
         DefaultFileSystemConfigBuilder.getInstance().getUserAuthenticator( getFileSystemOptions() );
@@ -65,6 +80,11 @@ public class S3FileSystem extends AbstractFileSystem implements FileSystem {
         UserAuthenticationData data = userAuthenticator.requestAuthentication( S3FileProvider.AUTHENTICATOR_TYPES );
         awsAccessKey = String.valueOf( data.getData( UserAuthenticationData.USERNAME ) );
         awsSecretKey = String.valueOf( data.getData( UserAuthenticationData.PASSWORD ) );
+      } else if ( awsAccessKeySystemEnvValue != null && awsSecretKeySystemEnvValue != null ) {
+        awsAccessKey = awsAccessKeySystemEnvValue;
+        awsSecretKey = awsSecretKeySystemEnvValue;
+        awsAccessKeyCache = awsAccessKeySystemEnvValue;
+        awsSecretKeyCache = awsSecretKeySystemEnvValue;
       } else {
         awsAccessKey = ( (URLFileName) getRootName() ).getUserName();
         awsSecretKey = ( (URLFileName) getRootName() ).getPassword();
@@ -80,4 +100,20 @@ public class S3FileSystem extends AbstractFileSystem implements FileSystem {
     return service;
   }
 
+  private boolean isEmpty( String value ) {
+    return value == null || value.length() <= 0;
+  }
+
+  private boolean hasChanged( String previousValue, String currentValue ) {
+    if ( !isEmpty( previousValue ) && isEmpty( currentValue ) ) {
+      return true;
+    }
+    if ( isEmpty( previousValue ) && !isEmpty( currentValue ) ) {
+      return true;
+    }
+    if ( !isEmpty( previousValue ) && !isEmpty( currentValue ) && !currentValue.equals( previousValue ) ) {
+      return true;
+    }
+    return false;
+  }
 }
