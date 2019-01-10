8e388de7632703135735e5d4106678c183c20f48
Kurtis Walker <kurtis.walker@hitachivantara.com>
2019-01-10 14:02:25 -0500

    [BACKLOG-26921] - implementing step analyzer for kafka producer

---
+++ kettle-plugins/kafka/src/main/java/org/pentaho/big/data/kettle/plugins/kafka/KafkaProducerERC.java
@@ -0,0 +1,41 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2019 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+package org.pentaho.big.data.kettle.plugins.kafka;
+
+import org.pentaho.metaverse.api.IAnalysisContext;
+import org.pentaho.metaverse.api.analyzer.kettle.step.BaseStepExternalResourceConsumer;
+import org.pentaho.metaverse.api.model.IExternalResourceInfo;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class KafkaProducerERC extends BaseStepExternalResourceConsumer {
+  @Override public Collection<IExternalResourceInfo> getResourcesFromMeta( Object consumer, IAnalysisContext context ) {
+    KafkaProducerOutputMeta meta = (KafkaProducerOutputMeta) consumer;
+    KafkaResourceInfo kafkaResourceInfo = new KafkaResourceInfo( meta.getBootstrapServers(), meta.getTopic() );
+    return Collections.singleton( kafkaResourceInfo );
+  }
+
+  @Override public Class getMetaClass() {
+    return KafkaProducerOutputMeta.class;
+  }
+}
