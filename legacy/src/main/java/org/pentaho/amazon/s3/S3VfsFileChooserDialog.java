99c16fc0528d99c4e058ae46ce4bd2ab80512339
ssamora <sergiosamora@gmail.com>
2019-03-25 19:21:32 +0000

    [PDI-17450] S3 File Output cannot browse s3 buckets after upgrade from 7.1 to 8.1

--- legacy/src/main/java/org/pentaho/amazon/s3/S3VfsFileChooserDialog.java
+++ legacy/src/main/java/org/pentaho/amazon/s3/S3VfsFileChooserDialog.java
@@ -22,101 +22,34 @@
 
 package org.pentaho.amazon.s3;
 
-import com.amazonaws.auth.AWSCredentials;
 import org.apache.commons.vfs2.FileObject;
-import org.apache.commons.vfs2.FileSystemException;
-import org.apache.commons.vfs2.FileSystemOptions;
-import org.apache.commons.vfs2.auth.StaticUserAuthenticator;
-import org.apache.commons.vfs2.impl.DefaultFileSystemConfigBuilder;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridLayout;
 import org.pentaho.amazon.AmazonS3FileSystemBootstrap;
-import org.pentaho.amazon.AmazonSpoonPlugin;
-import org.pentaho.di.core.exception.KettleFileException;
-import org.pentaho.di.core.logging.LogChannel;
-import org.pentaho.di.core.variables.VariableSpace;
-import org.pentaho.di.core.variables.Variables;
-import org.pentaho.di.core.vfs.KettleVFS;
-import org.pentaho.di.ui.spoon.Spoon;
 import org.pentaho.s3.vfs.S3FileProvider;
-import org.pentaho.vfs.ui.CustomVfsUiPanel;
 import org.pentaho.vfs.ui.VfsFileChooserDialog;
 
 /**
  * The UI for S3 VFS
  */
-public class S3VfsFileChooserDialog extends CustomVfsUiPanel {
-
-  private static Class<?> PKG = AmazonSpoonPlugin.class;
-
-  private LogChannel log = new LogChannel( this );
-
-  private FileObject rootFile;
-  private FileObject initialFile;
-  private VfsFileChooserDialog vfsFileChooserDialog;
+public class S3VfsFileChooserDialog extends S3VfsFileChooserBaseDialog {
 
   public S3VfsFileChooserDialog( VfsFileChooserDialog vfsFileChooserDialog, FileObject rootFile,
                                  FileObject initialFile ) {
-    super( S3FileProvider.SCHEME, AmazonS3FileSystemBootstrap.getS3FileSystemDisplayText(), vfsFileChooserDialog,
-      SWT.NONE );
-
-    this.vfsFileChooserDialog = vfsFileChooserDialog;
-    this.rootFile = rootFile;
-    this.initialFile = initialFile;
-
-    setLayout( new GridLayout() );
+    super( vfsFileChooserDialog, rootFile, initialFile, S3FileProvider.SCHEME, AmazonS3FileSystemBootstrap.getS3FileSystemDisplayText() );
   }
 
   @Override
   public void activate() {
-    vfsFileChooserDialog.setRootFile( rootFile );
-    vfsFileChooserDialog.setInitialFile( initialFile );
-    vfsFileChooserDialog.openFileCombo.setText( "s3://" );
-    vfsFileChooserDialog.vfsBrowser.fileSystemTree.removeAll();
+    vfsFileChooserDialog.openFileCombo.setText( "s3://s3/" );
     super.activate();
-
-    try {
-      FileObject newRoot = resolveFile( vfsFileChooserDialog.openFileCombo.getText() );
-      vfsFileChooserDialog.vfsBrowser.resetVfsRoot( newRoot );
-    } catch ( FileSystemException ignored ) {
-      //ignored
-    }
+  }
+  /**
+   * Build a URL given Url and Port provided by the user.
+   *
+   * @return
+   * @TODO: relocate to a s3 helper class or similar
+   */
+  public String buildS3FileSystemUrlString() {
+    return S3FileProvider.SCHEME + "://s3/";
   }
 
-  public FileObject resolveFile( String fileUri ) throws FileSystemException {
-    try {
-      return KettleVFS.getFileObject( fileUri, getVariableSpace(), getFileSystemOptions() );
-    } catch ( KettleFileException e ) {
-      throw new FileSystemException( e );
-    }
-  }
-
-  public FileObject resolveFile( String fileUri, FileSystemOptions opts ) throws FileSystemException {
-    try {
-      return KettleVFS.getFileObject( fileUri, getVariableSpace(), opts );
-    } catch ( KettleFileException e ) {
-      throw new FileSystemException( e );
-    }
-  }
-
-  private FileSystemOptions getFileSystemOptions() throws FileSystemException {
-    FileSystemOptions opts = new FileSystemOptions();
-    AWSCredentials credentials = S3CredentialsProvider.getAWSCredentials();
-    if ( credentials != null ) {
-      StaticUserAuthenticator userAuthenticator =
-        new StaticUserAuthenticator( null, credentials.getAWSAccessKeyId(), credentials.getAWSSecretKey() );
-      DefaultFileSystemConfigBuilder.getInstance().setUserAuthenticator( opts, userAuthenticator );
-    }
-    return opts;
-  }
-
-  private VariableSpace getVariableSpace() {
-    if ( Spoon.getInstance().getActiveTransformation() != null ) {
-      return Spoon.getInstance().getActiveTransformation();
-    } else if ( Spoon.getInstance().getActiveJob() != null ) {
-      return Spoon.getInstance().getActiveJob();
-    } else {
-      return new Variables();
-    }
-  }
 }
