1d1a861723d4eab2a03247da2105eeda37b38c26
Brandon Groves <brandon.groves@gmail.com>
2019-04-02 10:31:28 -0400

    [BACKLOG-27362] Move big data formats getFields method for pentaho-ee testing (#1653)
    Avro already had getFields in the base class but Orc and Parquet only
    had them in the impl class. This will help with testing on dependant
    projects.

--- kettle-plugins/formats/src/main/java/org/pentaho/big/data/kettle/plugins/formats/impl/orc/input/OrcInputMeta.java
+++ kettle-plugins/formats/src/main/java/org/pentaho/big/data/kettle/plugins/formats/impl/orc/input/OrcInputMeta.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2018 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2018-2019 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -26,21 +26,12 @@ import org.pentaho.big.data.api.cluster.NamedClusterService;
 import org.pentaho.big.data.api.cluster.service.locator.NamedClusterServiceLocator;
 import org.pentaho.big.data.kettle.plugins.formats.orc.input.OrcInputMetaBase;
 import org.pentaho.di.core.annotations.Step;
-import org.pentaho.di.core.exception.KettlePluginException;
-import org.pentaho.di.core.exception.KettleStepException;
 import org.pentaho.di.core.injection.InjectionSupported;
-import org.pentaho.di.core.row.RowMetaInterface;
-import org.pentaho.di.core.row.ValueMetaInterface;
-import org.pentaho.di.core.row.value.ValueMetaFactory;
-import org.pentaho.di.core.variables.VariableSpace;
-import org.pentaho.di.repository.Repository;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.StepDataInterface;
 import org.pentaho.di.trans.step.StepInterface;
 import org.pentaho.di.trans.step.StepMeta;
-import org.pentaho.hadoop.shim.api.format.IOrcInputField;
-import org.pentaho.metastore.api.IMetaStore;
 
 //keep ID as new because we will have old step with ID OrcInput
 @Step( id = "OrcInput", image = "OI.svg", name = "OrcInput.Name", description = "OrcInput.Description",
@@ -88,35 +79,5 @@ public class OrcInputMeta extends OrcInputMetaBase {
 
   public NamedClusterServiceLocator getNamedClusterServiceLocator() {
     return namedClusterServiceLocator;
-  }
-
-  @Override
-  public void getFields( RowMetaInterface rowMeta, String origin, RowMetaInterface[] info, StepMeta nextStep,
-                        VariableSpace space, Repository repository, IMetaStore metaStore ) throws
-          KettleStepException {
-    try {
-      if ( !inputFiles.passingThruFields ) {
-        // all incoming fields are not transmitted !
-        rowMeta.clear();
-      } else {
-        if ( info != null ) {
-          boolean found = false;
-          for ( int i = 0; i < info.length && !found; i++ ) {
-            if ( info[i] != null ) {
-              rowMeta.mergeRowMeta( info[i], origin );
-              found = true;
-            }
-          }
-        }
-      }
-      for ( IOrcInputField field : getInputFields() ) {
-        String value = space.environmentSubstitute( field.getPentahoFieldName() );
-        ValueMetaInterface v = ValueMetaFactory.createValueMeta( value, field.getPentahoType() );
-        v.setOrigin( origin );
-        rowMeta.addValueMeta( v );
-      }
-    } catch ( KettlePluginException e ) {
-      throw new KettleStepException( "Unable to create value type", e );
-    }
   }
 }
