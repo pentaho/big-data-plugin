d64c07ef84d9bac715595c1bd620f02022de7276
James Ricks <ricksjames@gmail.com>
2019-09-05 16:38:16 -0400

    [BACKLOG-27754] Initial commit of thin hadoop cluster UI

---
+++ kettle-plugins/hadoop-cluster/ui/src/main/java/org/pentaho/big/data/kettle/plugins/hadoopcluster/ui/endpoints/HadoopClusterEndpoints.java
@@ -0,0 +1,58 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2019 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.big.data.kettle.plugins.hadoopcluster.ui.endpoints;
+
+import org.pentaho.big.data.kettle.plugins.hadoopcluster.ui.dialog.HadoopClusterDialog;
+import org.pentaho.di.core.Const;
+import org.pentaho.di.i18n.BaseMessages;
+import org.pentaho.di.ui.spoon.Spoon;
+import org.pentaho.osgi.metastore.locator.api.MetastoreLocator;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.util.function.Supplier;
+
+import org.pentaho.di.ui.util.HelpUtils;
+
+public class HadoopClusterEndpoints {
+
+  private static Class<?> PKG = HadoopClusterDialog.class;
+  private Supplier<Spoon> spoonSupplier = Spoon::getInstance;
+
+  public static final String HELP_URL =
+    Const.getDocUrl( BaseMessages.getString( PKG, "HadoopCluster.help.dialog.Help" ) );
+
+  public HadoopClusterEndpoints( MetastoreLocator metastoreLocator ) {
+  }
+
+  @GET
+  @Path( "/help" )
+  public Response help() {
+    spoonSupplier.get().getShell().getDisplay().asyncExec( () ->
+      HelpUtils.openHelpDialog( spoonSupplier.get().getDisplay().getActiveShell(),
+        BaseMessages.getString( PKG, "HadoopCluster.help.dialog.Title" ),
+        HELP_URL, BaseMessages.getString( PKG, "HadoopCluster.help.dialog.Header" ) ) );
+    return Response.ok().build();
+  }
+}
