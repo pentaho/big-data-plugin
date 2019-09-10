95b0951848e99aeec063df9f44efb489ea21ac0d
Jacob Gminder <jacob.gminder@hitachivantara.com>
2019-09-10 07:26:47 -0400

    Revert "[BACKLOG-27754] Hadoop Cluster UX - Create New Unsecured Cluster Configuration from Site XML."
    This reverts commit 11b81598df2df763aa1e7c616948c469f52f8fd2.

--- kettle-plugins/hadoop-cluster/ui/src/main/java/org/pentaho/big/data/kettle/plugins/hadoopcluster/ui/endpoints/HadoopClusterEndpoints.java
+++ kettle-plugins/hadoop-cluster/ui/src/main/java/org/pentaho/big/data/kettle/plugins/hadoopcluster/ui/endpoints/HadoopClusterEndpoints.java
@@ -23,33 +23,27 @@
 package org.pentaho.big.data.kettle.plugins.hadoopcluster.ui.endpoints;
 
 import org.pentaho.big.data.kettle.plugins.hadoopcluster.ui.dialog.HadoopClusterDialog;
-import org.pentaho.di.base.AbstractMeta;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.i18n.BaseMessages;
 import org.pentaho.di.ui.spoon.Spoon;
 import org.pentaho.osgi.metastore.locator.api.MetastoreLocator;
-import org.pentaho.hadoop.shim.api.cluster.NamedClusterService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import java.util.function.Supplier;
 
 import org.pentaho.di.ui.util.HelpUtils;
-import org.pentaho.big.data.plugins.common.ui.MultishimNamedCluster;
 
 public class HadoopClusterEndpoints {
 
   private static Class<?> PKG = HadoopClusterDialog.class;
   private Supplier<Spoon> spoonSupplier = Spoon::getInstance;
-  private NamedClusterService namedClusterService;
 
   public static final String HELP_URL =
     Const.getDocUrl( BaseMessages.getString( PKG, "HadoopCluster.help.dialog.Help" ) );
 
-  public HadoopClusterEndpoints( MetastoreLocator metastoreLocator, NamedClusterService namedClusterService ) {
-    this.namedClusterService = namedClusterService;
+  public HadoopClusterEndpoints( MetastoreLocator metastoreLocator ) {
   }
 
   @GET
@@ -60,16 +54,5 @@ public class HadoopClusterEndpoints {
         BaseMessages.getString( PKG, "HadoopCluster.help.dialog.Title" ),
         HELP_URL, BaseMessages.getString( PKG, "HadoopCluster.help.dialog.Header" ) ) );
     return Response.ok().build();
-  }
-
-  //http://localhost:9051/cxf/hadoop-cluster/newNamedCluster?name=testName
-  @GET
-  @Path( "/newNamedCluster" )
-  public Response newNamedCluster( @QueryParam( "name" ) String name ) {
-    Spoon spoon = Spoon.getInstance();
-    AbstractMeta meta = (AbstractMeta) spoon.getActiveMeta();
-    MultishimNamedCluster cl = new MultishimNamedCluster( meta, spoon.getMetaStore(), this.namedClusterService );
-    String result = cl.newNamedCluster( name );
-    return Response.ok( result ).build();
   }
 }
