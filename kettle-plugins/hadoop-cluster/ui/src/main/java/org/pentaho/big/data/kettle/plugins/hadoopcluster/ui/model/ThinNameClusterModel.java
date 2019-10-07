981e445fd67d78d941b7a25bf203bd0ee300a327
e-cuellar <ezequiel.cuellar-ojeda@hitachivantara.com>
2019-10-07 11:57:29 -0400

    [BACKLOG-31382] Hadoop Cluster UX - Create New Cluster in Thin Dialog

---
+++ kettle-plugins/hadoop-cluster/ui/src/main/java/org/pentaho/big/data/kettle/plugins/hadoopcluster/ui/model/ThinNameClusterModel.java
@@ -0,0 +1,135 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2019 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.big.data.kettle.plugins.hadoopcluster.ui.model;
+
+public class ThinNameClusterModel {
+  private String name;
+  private String shimIdentifier;
+  private String hdfsHost;
+  private String hdfsPort;
+  private String hdfsUsername;
+  private String hdfsPassword;
+  private String jobTrackerHost;
+  private String jobTrackerPort;
+  private String zooKeeperHost;
+  private String zooKeeperPort;
+  private String oozieUrl;
+  private String kafkaBootstrapServers;
+
+  public String getHdfsHost() {
+    return hdfsHost;
+  }
+
+  public void setHdfsHost( String hdfsHost ) {
+    this.hdfsHost = hdfsHost;
+  }
+
+  public String getHdfsPort() {
+    return hdfsPort;
+  }
+
+  public void setHdfsPort( String hdfsPort ) {
+    this.hdfsPort = hdfsPort;
+  }
+
+  public String getHdfsUsername() {
+    return hdfsUsername;
+  }
+
+  public void setHdfsUsername( String hdfsUsername ) {
+    this.hdfsUsername = hdfsUsername;
+  }
+
+  public String getHdfsPassword() {
+    return hdfsPassword;
+  }
+
+  public void setHdfsPassword( String hdfsPassword ) {
+    this.hdfsPassword = hdfsPassword;
+  }
+
+  public String getJobTrackerHost() {
+    return jobTrackerHost;
+  }
+
+  public void setJobTrackerHost( String jobTrackerHost ) {
+    this.jobTrackerHost = jobTrackerHost;
+  }
+
+  public String getJobTrackerPort() {
+    return jobTrackerPort;
+  }
+
+  public void setJobTrackerPort( String jobTrackerPort ) {
+    this.jobTrackerPort = jobTrackerPort;
+  }
+
+  public String getZooKeeperHost() {
+    return zooKeeperHost;
+  }
+
+  public void setZooKeeperHost( String zooKeeperHost ) {
+    this.zooKeeperHost = zooKeeperHost;
+  }
+
+  public String getZooKeeperPort() {
+    return zooKeeperPort;
+  }
+
+  public void setZooKeeperPort( String zooKeeperPort ) {
+    this.zooKeeperPort = zooKeeperPort;
+  }
+
+  public String getKafkaBootstrapServers() {
+    return kafkaBootstrapServers;
+  }
+
+  public void setKafkaBootstrapServers( String kafkaBootstrapServers ) {
+    this.kafkaBootstrapServers = kafkaBootstrapServers;
+  }
+
+  public String getShimIdentifier() {
+    return shimIdentifier;
+  }
+
+  public void setShimIdentifier( String shimIdentifier ) {
+    this.shimIdentifier = shimIdentifier;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName( String name ) {
+    this.name = name;
+  }
+
+  public String getOozieUrl() {
+    return oozieUrl;
+  }
+
+  public void setOozieUrl( String oozieUrl ) {
+    this.oozieUrl = oozieUrl;
+  }
+
+}
