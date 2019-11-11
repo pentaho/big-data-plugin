5c6cf3c5798e5afe3df3e47c6516e920d78bd32e
ddiroma <dean.diroma@hitachivantara.com>
2019-11-11 13:43:40 -0500

    [BACKLOG-31963] Add support for s3a URLs to PDI

---
+++ legacy/src/main/java/org/pentaho/amazon/s3/S3AVfsFileChooserDialog.java
@@ -0,0 +1,55 @@
+/******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2019 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.amazon.s3;
+
+import org.apache.commons.vfs2.FileObject;
+import org.pentaho.amazon.AmazonS3AFileSystemBootstrap;
+import org.pentaho.s3a.vfs.S3AFileProvider;
+import org.pentaho.vfs.ui.VfsFileChooserDialog;
+
+/**
+ * The UI for S3 VFS
+ */
+public class S3AVfsFileChooserDialog extends S3VfsFileChooserBaseDialog {
+
+  public S3AVfsFileChooserDialog( VfsFileChooserDialog vfsFileChooserDialog, FileObject rootFile,
+                                  FileObject initialFile ) {
+    super( vfsFileChooserDialog, rootFile, initialFile, S3AFileProvider.SCHEME, AmazonS3AFileSystemBootstrap.getS3AFileSystemDisplayText() );
+  }
+
+  @Override
+  public void activate() {
+    vfsFileChooserDialog.openFileCombo.setText( "s3a://s3a/" );
+    super.activate();
+  }
+
+  /**
+   * Build a URL given Url and Port provided by the user.
+   *
+   * @return
+   */
+  @Override
+  public String buildS3FileSystemUrlString() {
+    return S3AFileProvider.SCHEME + "://s3a/";
+  }
+}
