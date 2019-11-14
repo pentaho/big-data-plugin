aa395fe50b814e1d42157d7b2f5cc1e1f434e0c8
Daniel Moore <daniel.moore@hitachivantara.com>
2019-11-14 15:35:36 -0500

    [BACKLOG-29786][WIP] adds new database connection type

---
+++ kettle-plugins/hive/src/main/java/org/pentaho/big/data/kettle/plugins/hive/HiveWarehouseDatabaseMeta.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2019 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+package org.pentaho.big.data.kettle.plugins.hive;
+
+import org.pentaho.di.core.plugins.DatabaseMetaPlugin;
+import org.pentaho.hadoop.shim.api.cluster.NamedClusterService;
+import org.pentaho.hadoop.shim.api.jdbc.DriverLocator;
+import org.pentaho.osgi.metastore.locator.api.MetastoreLocator;
+
+@DatabaseMetaPlugin( type = "HIVEWAREHOUSE", typeDescription = "Hive Warehouse Connector" )
+public class HiveWarehouseDatabaseMeta extends Hive2DatabaseMeta {
+  public HiveWarehouseDatabaseMeta( DriverLocator driverLocator, NamedClusterService namedClusterService, MetastoreLocator metastoreLocator ) {
+    super( driverLocator, namedClusterService, metastoreLocator );
+  }
+}
