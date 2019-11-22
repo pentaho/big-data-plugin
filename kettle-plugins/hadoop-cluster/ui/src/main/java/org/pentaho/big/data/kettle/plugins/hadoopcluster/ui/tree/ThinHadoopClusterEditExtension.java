8a3c2b6c9a165432d83e9d9e623da06f20584af3
ezecuellarojeda <ezequiel.cuellar-ojeda@hitachivantara.com>
2019-11-22 15:09:10 -0500

    [BACKLOG-32325] Hadoop Cluster UX - Double Click doesn't edit Cluster

---
+++ kettle-plugins/hadoop-cluster/ui/src/main/java/org/pentaho/big/data/kettle/plugins/hadoopcluster/ui/tree/ThinHadoopClusterEditExtension.java
@@ -0,0 +1,65 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2019 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+package org.pentaho.big.data.kettle.plugins.hadoopcluster.ui.tree;
+
+import com.google.common.collect.ImmutableMap;
+import org.pentaho.big.data.kettle.plugins.hadoopcluster.ui.dialog.HadoopClusterDelegate;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.extension.ExtensionPoint;
+import org.pentaho.di.core.extension.ExtensionPointInterface;
+import org.pentaho.di.core.logging.LogChannelInterface;
+import org.pentaho.di.core.namedcluster.model.NamedCluster;
+import org.pentaho.di.ui.spoon.SelectionTreeExtension;
+import org.pentaho.di.ui.spoon.Spoon;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@ExtensionPoint( id = "ThinHadoopClusterEditExtension", description = "Edits named cluster",
+  extensionPointId = "SpoonViewTreeExtension" )
+public class ThinHadoopClusterEditExtension implements ExtensionPointInterface {
+
+  HadoopClusterDelegate hadoopClusterDelegate;
+  private static final Logger logChannel = LoggerFactory.getLogger( ThinHadoopClusterEditExtension.class );
+
+  public ThinHadoopClusterEditExtension( HadoopClusterDelegate hadoopClusterDelegate ) {
+    this.hadoopClusterDelegate = hadoopClusterDelegate;
+  }
+
+  public void callExtensionPoint( LogChannelInterface log, Object extension ) throws KettleException {
+    try {
+      SelectionTreeExtension selectionTreeExtension = (SelectionTreeExtension) extension;
+      if ( selectionTreeExtension.getAction().equals( Spoon.EDIT_SELECTION_EXTENSION ) ) {
+        Object selection = selectionTreeExtension.getSelection();
+        if ( selection instanceof NamedCluster ) {
+          NamedCluster namedCluster = (NamedCluster) selection;
+          String name = URLEncoder.encode( namedCluster.getName(), "UTF-8" );
+          hadoopClusterDelegate.openDialog( "new-edit", ImmutableMap.of( "name", name ) );
+        }
+      }
+    } catch ( UnsupportedEncodingException e ) {
+      logChannel.error( e.getMessage() );
+    }
+  }
+}
