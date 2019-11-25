c7c13f3f8859b043aaae77e58c9206067c7db90f
James Ricks <ricksjames@gmail.com>
2019-11-25 13:57:18 -0500

    [BACKLOG-31989] Reverting some changes to HadoopSpoonPlugin and removing legacy hadoop cluster tree extension point

--- legacy/src/main/java/org/pentaho/di/core/hadoop/HadoopSpoonPlugin.java
+++ legacy/src/main/java/org/pentaho/di/core/hadoop/HadoopSpoonPlugin.java
@@ -22,12 +22,25 @@
 
 package org.pentaho.di.core.hadoop;
 
+import org.pentaho.di.core.annotations.LifecyclePlugin;
 import org.pentaho.di.core.gui.GUIOption;
+import org.pentaho.di.core.lifecycle.LifeEventHandler;
+import org.pentaho.di.core.lifecycle.LifecycleListener;
 
-public class HadoopSpoonPlugin implements GUIOption<Object> {
+@LifecyclePlugin( id = "HadoopSpoonPlugin", name = "Hadoop Spoon Plugin" )
+public class HadoopSpoonPlugin implements LifecycleListener, GUIOption<Object> {
+  public static final String PLUGIN_ID = "HadoopSpoonPlugin";
 
   public static final String HDFS_SCHEME = "hdfs";
   public static final String MAPRFS_SCHEME = "maprfs";
+
+  public void onStart( LifeEventHandler arg0 ) {
+    //TODO: This class should be removed as a LifecyclePlugin and no longer implement LifecycleListener once dependencies are cleaned up
+  }
+
+  public void onExit( LifeEventHandler arg0 ) {
+    //TODO: This class should be removed as a LifecyclePlugin and no longer implement LifecycleListener once dependencies are cleaned up
+  }
 
   public String getLabelText() {
     return null;
