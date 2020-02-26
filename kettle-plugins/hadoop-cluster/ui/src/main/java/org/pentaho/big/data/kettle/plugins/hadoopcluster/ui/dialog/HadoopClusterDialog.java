35ddf8aa81375643a95b130395533787a9c8b3a6
ddiroma <dean.diroma@hitachivantara.com>
2020-02-26 11:12:55 -0500

    [BACKLOG-33406] Upgrade Angular JS library from 1.5.8 version to 1.7.8 for big-data-plugin--hadoop clusters

--- kettle-plugins/hadoop-cluster/ui/src/main/java/org/pentaho/big/data/kettle/plugins/hadoopcluster/ui/dialog/HadoopClusterDialog.java
+++ kettle-plugins/hadoop-cluster/ui/src/main/java/org/pentaho/big/data/kettle/plugins/hadoopcluster/ui/dialog/HadoopClusterDialog.java
@@ -65,7 +65,7 @@ public class HadoopClusterDialog extends ThinDialog {
 
     StringBuilder clientPath = new StringBuilder();
     clientPath.append( getClientPath() );
-    clientPath.append( "#/" );
+    clientPath.append( "#!/" );
     if ( thinAppState != null ) {
       clientPath.append( thinAppState );
     }
