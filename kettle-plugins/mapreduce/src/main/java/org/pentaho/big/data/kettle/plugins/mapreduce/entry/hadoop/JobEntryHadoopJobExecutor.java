bbed3182b3bd455eb0ec31450a08f3d3dac42f8d
Peter Rinehart <peter.rinehart@hitachivantara.com>
2022-01-03 16:05:16 -0500

    [BISERVER-14773] Fixes for Sqoop tests

--- kettle-plugins/mapreduce/src/main/java/org/pentaho/big/data/kettle/plugins/mapreduce/entry/hadoop/JobEntryHadoopJobExecutor.java
+++ kettle-plugins/mapreduce/src/main/java/org/pentaho/big/data/kettle/plugins/mapreduce/entry/hadoop/JobEntryHadoopJobExecutor.java
@@ -72,6 +72,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
