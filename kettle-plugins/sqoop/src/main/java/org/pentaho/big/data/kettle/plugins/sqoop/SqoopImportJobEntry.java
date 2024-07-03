41a9f6677ae00a191857eeb3767ffdd5e16f521e
Sergio Ribeiro <sergio.ribeiro@hitachivantara.com>
2024-07-03 10:28:17 +0100

    [SP-6555] Update and validate in-product links in Pentaho 9.3.0.8
    Fix SqoopExport/SqoopImport

--- kettle-plugins/sqoop/src/main/java/org/pentaho/big/data/kettle/plugins/sqoop/SqoopImportJobEntry.java
+++ kettle-plugins/sqoop/src/main/java/org/pentaho/big/data/kettle/plugins/sqoop/SqoopImportJobEntry.java
@@ -37,7 +37,7 @@ import java.util.Properties;
 @JobEntry( id = "SqoopImport", name = "Sqoop.Import.PluginName", description = "Sqoop.Import.PluginDescription",
     categoryDescription = "i18n:org.pentaho.di.job:JobCategory.Category.BigData", image = "sqoop-import.svg",
     i18nPackageName = "org.pentaho.di.job.entries.sqoop", version = "1",
-    documentationUrl = " https://pentaho-community.atlassian.net/wiki/display/EAI/Sqoop+Import" )
+    documentationUrl = "mk-95pdia003/pdi-job-entries/sqoop-import" )
 public class SqoopImportJobEntry extends AbstractSqoopJobEntry<SqoopImportConfig> {
 
   public SqoopImportJobEntry( NamedClusterService namedClusterService,
