969d463ba744bfa0891a74ace7660d005a849098
Dean DiRoma <dean.diroma@hitachivantara.com>
2024-07-16 11:41:19 -0400

    [BAD-1946] Add EMR versions 5.36 and 7.0 to available EMR Release versions to Amazon Hive and Amazon EMR job executor job entries

--- legacy-amazon/src/test/java/org/pentaho/amazon/AbstractAmazonJobExecutorControllerTest.java
+++ legacy-amazon/src/test/java/org/pentaho/amazon/AbstractAmazonJobExecutorControllerTest.java
@@ -98,7 +98,7 @@ public class AbstractAmazonJobExecutorControllerTest {
   @Test
   public void testPopulateReleases_getValidCountOfEnumElements() throws Exception {
 
-    int expectedCountOfReleases = 32;
+    int expectedCountOfReleases = 34;
 
     AbstractModelList<String> listReleases = jobExecutorController.populateReleases();
 
@@ -108,7 +108,7 @@ public class AbstractAmazonJobExecutorControllerTest {
   @Test
   public void testPopulateReleases_setFirstEmrReleaseInJobEntry() throws Exception {
 
-    String expectedEmrRelease = "emr-5.11.0";
+    String expectedEmrRelease = "emr-7.0.0";
 
     AmazonHiveJobExecutor jobEntry = spy( new AmazonHiveJobExecutor() );
     AmazonHiveJobExecutorController hiveJobExecutorController =
@@ -140,7 +140,7 @@ public class AbstractAmazonJobExecutorControllerTest {
   @Test
   public void testPopulateReleases_addNewEmrReleaseToReleasesList() throws Exception {
 
-    int expectedCountOfReleases = 33;
+    int expectedCountOfReleases = 35;
 
     when( jobExecutorController.getJobEntry().getEmrRelease() ).thenReturn( "emr-5.12.0" );
     AbstractModelList<String> listReleases = jobExecutorController.populateReleases();
@@ -152,7 +152,7 @@ public class AbstractAmazonJobExecutorControllerTest {
   @Test
   public void testPopulateReleases_setEmrReleaseToFirstElementFromEmrReleasesList() throws Exception {
 
-    String expectedEmrRelease = "emr-5.11.0";
+    String expectedEmrRelease = "emr-7.0.0";
 
     AmazonHiveJobExecutor jobEntry = spy( new AmazonHiveJobExecutor() );
 
