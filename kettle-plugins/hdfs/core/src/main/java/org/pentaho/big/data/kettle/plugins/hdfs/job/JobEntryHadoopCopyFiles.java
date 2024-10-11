b490c9f06012690cc9eaab6774d8477f79ee1db7
Ezequiel Cuellar <ezequiel.cuellar-ojeda@hitachivantara.com>
2024-10-11 10:47:45 -0400

    [BACKLOG-36711] (10.X) -Move bigdata steps entries from OSGI to no-OSGi - Rebase against master

--- kettle-plugins/hdfs/core/src/main/java/org/pentaho/big/data/kettle/plugins/hdfs/job/JobEntryHadoopCopyFiles.java
+++ kettle-plugins/hdfs/core/src/main/java/org/pentaho/big/data/kettle/plugins/hdfs/job/JobEntryHadoopCopyFiles.java
@@ -56,6 +56,7 @@ public class JobEntryHadoopCopyFiles extends JobEntryCopyFiles {
     this.namedClusterService = NamedClusterManager.getInstance();
     this.runtimeTestActionService = RuntimeTestActionServiceImpl.getInstance();
     this.runtimeTester = RuntimeTesterImpl.getInstance();
+    this.fileFolderUrlMappings = new HashMap<>();
   }
 
   /**
