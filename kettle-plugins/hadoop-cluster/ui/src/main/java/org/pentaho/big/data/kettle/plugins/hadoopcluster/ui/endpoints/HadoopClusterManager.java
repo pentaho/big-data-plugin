c68ac779e33cc100f94f70461e2ca9f550b831f3
Ezequiel Cuellar <ezequiel.cuellar-ojeda@hitachivantara.com>
2025-07-07 13:45:31 -0400

    [BACKLOG-44522] Allow testing of a hadoop cluster using the apis from the Pentaho Server

--- kettle-plugins/hadoop-cluster/ui/src/main/java/org/pentaho/big/data/kettle/plugins/hadoopcluster/ui/endpoints/HadoopClusterManager.java
+++ kettle-plugins/hadoop-cluster/ui/src/main/java/org/pentaho/big/data/kettle/plugins/hadoopcluster/ui/endpoints/HadoopClusterManager.java
@@ -214,6 +214,11 @@ public class HadoopClusterManager implements RuntimeTestProgressCallback {
     this.internalShim = internalShim;
   }
 
+  public HadoopClusterManager( NamedClusterService namedClusterService, IMetaStore metaStore,
+                               String internalShim ) {
+    this( null, namedClusterService, metaStore, internalShim);
+  }
+
   public JSONObject importNamedCluster( ThinNameClusterModel model,
                                         Map<String, CachedFileItemStream> siteFilesSource ) {
     JSONObject response = new JSONObject();
