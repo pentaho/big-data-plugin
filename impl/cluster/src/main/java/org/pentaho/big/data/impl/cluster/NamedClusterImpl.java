0e7b9c86cefcfe8094971998099df4d3b8a60d34
joana-fb <132680682+joana-fb@users.noreply.github.com>
2025-10-03 15:29:53 +0100

    fix[PPP-5982]: add XMLConstants import to NamedClusterImpl

--- impl/cluster/src/main/java/org/pentaho/big/data/impl/cluster/NamedClusterImpl.java
+++ impl/cluster/src/main/java/org/pentaho/big/data/impl/cluster/NamedClusterImpl.java
@@ -24,6 +24,7 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
